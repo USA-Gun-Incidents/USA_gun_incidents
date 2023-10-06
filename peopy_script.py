c01fc102abf4836cc59e4a4e5fbd95a3b2a116d9
GiacomoAru <giacomoaru01@gmail.com>
2023-10-06 20:58:51 +0200

    geopy all over again

--- peopy_script.py
+++ peopy_script.py
@@ -1,8 +1,16 @@
-#lib
-import numpy as np
+
+'''For those who want to contribute to data collection, 
+choose a correct index, put it as an integer in the first line of the config file,
+ and put your name in the variable NAME.
+
+the program should run smoothly without errors, it should save everything it imports, 
+and to stop it just do ctrl + c and save the state (it doesn't update the config though)
+
+it is important to create the config file before running it,
+look at mine for syntax and folders location'''
+
+
 import pandas as pd
-import scipy as stats
-import matplotlib.pyplot as plt
 from datetime import datetime
 import os
 from geopy.geocoders import Nominatim
@@ -10,15 +18,21 @@ import json
 import time
 import random
 
-DIR = cur_path = os.path.dirname(__file__) + '\\data\\geopy\\'
+
+#PUT NAME HERE
+#(STRING)
+NAME = 'giacomo'
+
+DIR = cur_path = os.path.dirname(__file__) + '\\data\\geopy\\' + NAME + '_'
 OUT_FILE = open(DIR + 'geopy.txt', 'a+')
 ERR_FILE = open(DIR + 'geopy_error.txt', 'a+')
-STATS = open(DIR + 'geopy_stats.txt', 'a+')
-CONFIG = open(DIR + 'geopy_config.txt', 'r')
+STATS_FILE = open(DIR + 'geopy_stats.txt', 'a+')
+CONFIG = open(DIR + 'geopy_config.txt', 'r+')
 
 start = int(CONFIG.readline())
 CONFIG.close()
 
+#seconds before retrying the request
 PAUSES = [0.1, 1, 4]
 if type(start) is not int:
     print('ERRORE CONFIG')
@@ -26,86 +40,101 @@ if type(start) is not int:
 else:
     print('started from: ' + str(start))
 
-OUT_FILE.write('#DATE: ' + str(datetime.now()) +'\t' + str(start) +  '\t ---------- \tSTART\t ----------\n')
-STATS.write('#DATE: ' + str(datetime.now()) +'\t' + str(start) +  '\t ---------- \tSTART\t ----------\n')
-ERR_FILE.write('#DATE: ' + str(datetime.now()) +'\t' + str(start) +  '\t ---------- \tSTART\t ----------\n')
+#inizializing files
+OUT_FILE.write('#DATE: ' + str(datetime.now()) +'\t' + str(start) +  '\t ---------------------- \tSTART\t ----------------------\n')
+STATS_FILE.write('#DATE: ' + str(datetime.now()) +'\t' + str(start) +  '\t ---------------------- \tSTART\t ----------------------\n')
+ERR_FILE.write('#DATE: ' + str(datetime.now()) +'\t' + str(start) +  '\t ---------------------- \tSTART\t ----------------------\n')
 
-#the dataset is probably read rightcon
+
+#i chose to not drop any row because they are approximately 250 <<<<<< total row
 inc = pd.read_csv(os.path.dirname(__file__) + '\\data\\incidents.csv', sep=',') 
 #inc.drop_duplicates(subset=['latitude', 'longitude'], inplace=True)
 
-
-geolocator = Nominatim(user_agent="DM_project")
+#i don't know if is needed to change the name????
+geolocator = Nominatim(user_agent="DM_project_" + NAME)
 
 stats_d = {'total_r':0, 'partial_r':0, 'total_em':0, 'partial_em':0, 'total_ef':0, 'partial_ef':0, 'rate':-1}
 stats_t = time.time()
 stats_ind = 0
 stats_rate_alpha = 0.2
-for index, row in inc.iloc[start:].iterrows():
 
-    er = 0
-    while er < 3:
-        try:
-            location = geolocator.reverse(str(row['latitude']) + ' ' + str(row['longitude'])).raw
-            json.dump([index, location], OUT_FILE)
-            OUT_FILE.write('\n')
+try:
+    for index, row in inc.iloc[start:].iterrows():
 
-            stats_d['total_r'] += 1
-            stats_d['partial_r'] += 1
+        er = 0
+        while er < 3:
+            try:
+                #request
+                location = geolocator.reverse(str(row['latitude']) + ' ' + str(row['longitude'])).raw
+                json.dump([index, location], OUT_FILE)
+                OUT_FILE.write('\n')
 
-            dummy = random.randrange(0, 100)
-            if dummy < 2:
-                print('SUPER PING PONG')
-            elif dummy < 51 :
-                print('ping')
-            else:
-                print('pong')
-            
-            er = 10
+                stats_d['total_r'] += 1
+                stats_d['partial_r'] += 1
 
-        except Exception as e:
-            print('ERR: ' + str(er))
-            time.sleep(PAUSES[er])
-            er += 1
-            if er >= 3:
-                stats_d['total_ef'] += 1
-                stats_d['partial_ef'] += 1
-                ERR_FILE.write('date: ' + str(datetime.now()) + ' index: ' + str(index) + ' coord: ' + str(row['latitude']) + ' ' + str(row['longitude']) + ' err: ' + str(e) + '\n')
-                ERR_FILE.flush()
-            else:
-                stats_d['total_em'] += 1
-                stats_d['partial_em'] += 1
-    
-    if stats_ind > 25:
+                dummy = random.randrange(0, 100)
+                if dummy < 2:
+                    print('SUPER PING PONG')
+                elif dummy < 51 :
+                    print('ping')
+                else:
+                    print('pong')
+                
+                er = 10
 
-        stats_t = time.time() - stats_t
-        if stats_d['rate'] == -1:
-            stats_d['rate'] = stats_d['partial_r']/stats_t
-        else:
-            stats_d['rate'] = (1 - stats_rate_alpha)*stats_d['rate'] + stats_rate_alpha*(stats_d['partial_r']/stats_t)
-
-
-        stats_ind = 0
-        json.dump(stats_d, STATS)
-        STATS.write('\n')
-        STATS.flush()
-        stats_t = time.time()
-
-        stats_d['partial_ef'] = 0
-        stats_d['partial_em'] = 0
-        stats_d['partial_r'] = 0
+            #let's try again up to 2 times
+            except Exception as e:
+                print('ERR: ' + str(er))
+                time.sleep(PAUSES[er])
+                er += 1
+                if er >= 3:
+                    stats_d['total_ef'] += 1
+                    stats_d['partial_ef'] += 1
+                    ERR_FILE.write('date: ' + str(datetime.now()) + ' index: ' + str(index) + ' coord: ' + str(row['latitude']) + ' ' + str(row['longitude']) + ' err: ' + str(e) + '\n')
+                    ERR_FILE.flush()
+                else:
+                    stats_d['total_em'] += 1
+                    stats_d['partial_em'] += 1
         
-        break
-    else:
-        stats_ind += 1
+        #just some stats
+        if stats_ind > 300:
+
+            stats_t = time.time() - stats_t
+            if stats_d['rate'] == -1:
+                stats_d['rate'] = stats_d['partial_r']/stats_t
+            else:
+                stats_d['rate'] = (1 - stats_rate_alpha)*stats_d['rate'] + stats_rate_alpha*(stats_d['partial_r']/stats_t)
 
 
+            stats_ind = 0
+            json.dump(stats_d, STATS_FILE)
+            STATS_FILE.write('\n')
+            STATS_FILE.flush()
+            stats_t = time.time()
+
+            stats_d['partial_ef'] = 0
+            stats_d['partial_em'] = 0
+            stats_d['partial_r'] = 0
+            
+        else:
+            stats_ind += 1
+
+except KeyboardInterrupt:
+    print('INTERRUPTION...')
 
 
-OUT_FILE.write('#DATE: ' + str(datetime.now()) +'\t' + str(start) +  '\t ---------------------- END ---------------------\n')
-STATS.write('#DATE: ' + str(datetime.now()) +'\t' + str(start) +  '\t ---------------------- END ---------------------\n')
-ERR_FILE.write('#DATE: ' + str(datetime.now()) +'\t' + str(start) +  '\t ---------------------- END ---------------------\n')
+#termination
+OUT_FILE.write('#DATE: ' + str(datetime.now()) + '\t ---------------------- END ---------------------\n')
+json.dump(stats_d, STATS_FILE)
+STATS_FILE.write('\n')
+STATS_FILE.write('#DATE: ' + str(datetime.now()) + '\t ---------------------- END ---------------------\n')
+ERR_FILE.write('#DATE: ' + str(datetime.now()) + '\t ---------------------- END ---------------------\n')
 
+#closing all open files
 OUT_FILE.close()
 ERR_FILE.close()
-STATS.close()
+STATS_FILE.close()
+
+
+#bye bye
+#ciao ciao
