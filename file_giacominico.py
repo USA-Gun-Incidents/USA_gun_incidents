f9aee5c13c46c78f55b05cda35ec7731516dd5ce
GiacomoAru <giacomoaru01@gmail.com>
2023-10-07 19:00:54 +0200

    Dates 2

--- file_giacominico.py
+++ file_giacominico.py
@@ -14,12 +14,14 @@ import pandas as pd
 import scipy.stats as stats
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
+import random
 
 # %%
 #the dataset is probably read right
 inc = pd.read_csv('data/incidents.csv', sep=',') 
 inc.drop_duplicates()
 inc.info()
+inc.head()
 
 # %%
 for col in inc:
@@ -30,16 +32,8 @@ for col in inc:
 # Syntax and data semantics check 
 
 # %%
-# corventing dates in a correct format
+inc.drop('notes', axis=1)
 inc['date'] = inc.apply(lambda row : pd.to_datetime(row['date'], format="%Y-%m-%d"), axis = 1)
-
-
-# %%
-inc_coded = {}
-inc_coded['date'] = inc['date']
-
-inc_coded = pd.DataFrame(inc_coded)
-inc_coded.head()
 
 # %% [markdown]
 # Date distribution before error correction
@@ -105,23 +99,6 @@ plt.yticks(ticks, labels)
 dates_data = get_box_plot_data('a', boxplot)
 
 
-# %%
-factors_paired = [(i,j) for i in inc.columns.values for j in inc.columns.values] 
-
-chi2, p_values =[], []
-
-for f in factors_paired:
-    if f[0] != f[1]:
-        chitest = chi2_contingency(pd.crosstab(df[f[0]], df[f[1]]))   
-        chi2.append(chitest[0])
-        p_values.append(chitest[1])
-    else:      # for same factor pair
-        chi2.append(0)
-        p_values.append(0)
-
-chi2 = np.array(chi2).reshape((23,23)) # shape it as a matrix
-chi2 = pd.DataFrame(chi2, index=df.columns.values, columns=df.columns.values) # then a df for convenience
-
 # %% [markdown]
 # Syntax and semantic check after applying different techniques
 
@@ -169,12 +146,46 @@ for ax in axs:
 print('Range data: ', min_date, ' - ', max_date)
 
 # %%
-def replace_with_(x):
-        if x['date'] > dates_data['upper_whisker'][0].to_datetime64(): 
-                return x['date'] - pd.DateOffset(years=10)
-        else: return x['date']
+def replace_with_random(x):
+        ret = x['date']
+        while ret > dates_data['upper_whisker'][0].to_datetime64(): 
+                ret = inc['date'][random.randrange(0, len(inc))]
+        return ret
 
 
-new_dates_2 = inc.apply(lambda row : subtract_ten_if(row), axis = 1)
+new_dates_2 = inc.apply(lambda row : replace_with_random(row), axis = 1)
+
+# %%
+# plot range data
+tot_row = len(inc.index)
+
+# one binth for every month in the range
+min_date = new_dates_2.min()
+max_date = new_dates_2.max()
+n_bin = int((max_date - min_date).days / 30)
+n_bin_2 = int(1 + (10/3)*math.log10(tot_row))
+
+freq = int(tot_row/n_bin)
+equal_freq_bins=new_dates_2.sort_values(ascending=False).quantile(np.arange(0,1, 1/n_bin)).to_list()
+equal_freq_bins2=new_dates_2.sort_values(ascending=False).quantile(np.arange(0,1, 1/n_bin_2)).to_list()
+
+fig, axs = plt.subplots(4, sharex=True, sharey=True)
+fig.set_figwidth(14)
+fig.suptitle('Sharing both axes')
+
+colors_palette = iter(mcolors.TABLEAU_COLORS)
+#natural binning
+axs[0].hist(new_dates_2, bins=n_bin, color=next(colors_palette), density=True)
+axs[1].hist(new_dates_2, bins=n_bin_2, color=next(colors_palette), density=True)
+#equal freq binning
+axs[2].hist(new_dates_2, bins=equal_freq_bins, color=next(colors_palette),  density=True)
+axs[3].hist(new_dates_2, bins=equal_freq_bins2, color=next(colors_palette),  density=True)
+for ax in axs:
+    ax.axvline(min_date, color='k', linestyle='dashed', linewidth=1)
+    ax.axvline(max_date, color='k', linestyle='dashed', linewidth=1)
+
+
+
+print('Range data: ', min_date, ' - ', max_date)
 
 
