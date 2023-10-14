0de5cf3d174574a3efa7541a949be993b69ba7ab
iretes <i.testa@studenti.unipi.it>
2023-10-14 23:05:43 +0200

    data understanding final notebook progress

--- TASK_1/1.1-Data_Understanding.py
+++ TASK_1/1.1-Data_Understanding.py
@@ -7,11 +7,20 @@
 # %%
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
 
 # %% [markdown]
 # We define constants and settings for the notebook:
 
 # %%
+import os
+import sys
+sys.path.append(os.path.abspath('..')) # TODO: c'è un modo per farlo meglio?
+from plot_utils import *
+
+%matplotlib inline
+
 DATA_FOLDER_PATH = '../data/'
 
 pd.set_option('display.max_columns', None)
@@ -156,7 +165,7 @@ poverty_state_year_size = poverty_data.groupby(['state', 'year']).size()
 poverty_state_year_size[poverty_state_year_size>1]
 
 # %% [markdown]
-# We display the data for Wyoming, the only one with this issue.
+# We display the data for Wyoming, the only one with this issue:
 
 # %%
 poverty_data[(poverty_data['state']=='Wyoming')]
@@ -176,14 +185,6 @@ poverty_data.at[571,'year']=2010
 # %% [markdown]
 # Since the tuple <`state`, `year`> uniquely identifies each row we can conclude that there are no missing rows.
 # 
-# We now count the number of duplicated rows.
-
-# %%
-poverty_data.duplicated().sum()
-
-# %% [markdown]
-# No duplicates are present. 
-# 
 # Now, we count how many rows have missing values:
 
 # %%
@@ -198,12 +199,65 @@ poverty_data[poverty_data['povertyPercentage'].isnull()]['year'].unique()
 # %% [markdown]
 # As expected we have no data from 2012. Later we will fix this issue.
 # 
-# Now we visualize the distribution of `povertyPercentage` for each state.
+# Now we visualize the distribution of poverty percentage for each state.
 
 # %%
-poverty_data.boxplot(column='povertyPercentage', by='state', figsize=(20, 10), rot=90)
+poverty_data.boxplot(column='povertyPercentage', by='state', figsize=(20, 10), rot=90, xlabel='state', ylabel='Poverty (%)')
+plt.suptitle('Poverty Percentage by State')
+plt.title('')
+plt.tight_layout()
 
 # %% [markdown]
+# This plot shows that Arkansas, Kentucky, Nebraska and North Dakota seems to be affected by fliers. We check this by plotting their poverty percentage over the years.
+
+# %%
+poverty_data[
+    poverty_data['state'].isin(['Arkansas', 'Kentucky', 'Nebraska', 'North Dakota', 'United States'])
+    ].pivot(index='year', columns='state', values='povertyPercentage').plot(kind='line')
+plt.legend()
+plt.xlabel('Year')
+plt.ylabel('Poverty (%)')
+plt.title('Poverty (%) over the years')
+
+# %% [markdown]
+# The plot above shows that actually those values are not outliers. TODO: migliorare questo commento
+
+# %%
+poverty_data.groupby('year')['povertyPercentage'].mean().plot(kind='line', figsize=(15, 5), label='USA average', color='black', style='--')
+plt.fill_between(
+    poverty_data.groupby('year')['povertyPercentage'].mean().index,
+    poverty_data.groupby('year')['povertyPercentage'].mean() - poverty_data.groupby('year')['povertyPercentage'].std(),
+    poverty_data.groupby('year')['povertyPercentage'].mean() + poverty_data.groupby('year')['povertyPercentage'].std(),
+    alpha=0.2,
+    color='gray'
+)
+
+# %% [markdown]
+# We now plot the average poverty percentage over the years for each state:
+
+# %%
+poverty_data.groupby(['state'])['povertyPercentage'].mean().sort_values().plot(kind='bar', figsize=(15, 5))
+plt.title(f'Average Poverty (%) in the period {poverty_data.year.min()}-{poverty_data.year.max()}')
+plt.xlabel('State')
+plt.ylabel('Average Poverty (%)')
+
+# %% [markdown]
+# It is evident that New Hampshire's average poverty rate is markedly lower than that of the other states, whereas Mississippi's average poverty rate is notably higher than the rest. 
+# 
+# To inspect and compare the poverty percentage of each state over the year, we plot an interactive line chart:
+
+# %%
+fig = px.line(
+    poverty_data.pivot(index='year', columns='state', values='povertyPercentage'),
+    title='Poverty percentage in the US over the years')
+fig.show()
+
+# %% [markdown]
+# TODO: commentare
+
+# %% [markdown]
+# TODO: spostare in preparation.
+# 
 # To imputate the missing data from 2012, we calculate the average of the `povertyPercentage` values for the preceding and succeeding year.
 
 # %%
@@ -212,18 +266,71 @@ poverty_data['povertyPercentage'] = poverty_data.apply(
     lambda x: poverty_perc_2012[x['state']] if x['year']==2012 else x['povertyPercentage'], axis=1
 )
 
-# %%
-# plot bar plot of povertyPercentage for each state sorting by povertyPercentage
-poverty_data.groupby(['state'])['povertyPercentage'].mean().sort_values().plot(kind='bar', figsize=(15, 5))
+# %% [markdown]
+# Now we plot again the interactive line chart:
 
 # %%
-# mappe di povertà sequenziali
+fig = px.line(
+    poverty_data.pivot(index='year', columns='state', values='povertyPercentage'),
+    title='Poverty percentage in the US over the years')
+fig.show()
+
+# %% [markdown]
+# We also visualize how the poverty percentage changed with an animated map:
 
 # %%
-# come varia la povertà media
+poverty_data.sort_values(by=['state', 'year'], inplace=True)
+poverty_data['px_code'] = poverty_data['state'].map(usa_code) # retrieve the code associated to each state (the map is defined in the file utils.py)
+fig = px.choropleth(
+    poverty_data[poverty_data['state']!='United States'],
+    locations='px_code',
+    locationmode="USA-states",
+    color='povertyPercentage',
+    color_continuous_scale="rdbu",
+    range_color=(
+        min(poverty_data[poverty_data['state']!='United States']['povertyPercentage']),
+        max(poverty_data[poverty_data['state']!='United States']['povertyPercentage'])),
+    scope="usa",
+    animation_frame='year',
+    title="US Poverty Percentage over the years",
+    hover_name='state',
+    hover_data={'px_code': False}
+)
+fig.update_layout(
+    title_text='US Poverty Percentage over the years',
+    coloraxis_colorbar_title_text = 'Poverty (%)'
+)
+fig.show()
 
 # %%
-# come visualizzare curve? (ordina in base alla media e fai un istogramma per anno?)
+# TODO: usare unica color bar e aggiustare dimensioni in modo che si leggano gli stati (questa versione potrebbe servire per il report)
+
+fig, axs = plt.subplots(ncols=3, nrows=6, figsize=(30, 40))
+vmin, vmax = poverty_data['povertyPercentage'].agg(['min', 'max'])
+
+row_count = 0
+col_count = 0
+for year in poverty_data['year'].unique():
+    plot_usa_map(
+        poverty_data[poverty_data['year']==year],
+        col_to_plot='povertyPercentage',
+        ax=axs[row_count][col_count],
+        state_col='state',
+        vmin=vmin,
+        vmax=vmax,
+        title=str(year),
+        cbar_title="Poverty (%)",
+        cmap='RdBu',
+        borders_path="../cb_2018_us_state_500k"
+    )
+    col_count += 1
+    if col_count == 3:
+        col_count = 0
+        row_count += 1
+
+fig.delaxes(axs[5][2])
+fig.suptitle("Povery percentage over the years", fontsize=25)
+fig.tight_layout()
 
 # %% [markdown]
 # ## Elections Data
@@ -264,7 +371,7 @@ elections_data.info()
 # %% [markdown]
 # We notice that:
 # - the inferred types are correct
-# - there are no missing values (we still should assess if rows are missing for specific year/states/congressional districts)
+# - there are no missing values (however, we should still assess whether there are any missing rows for specific years, states, or congressional districts)
 
 # %% [markdown]
 # We display descriptive statistics:
@@ -277,5 +384,157 @@ elections_data.describe(include='all')
 # - year spans from 2004 to 2020
 # - there are 6 unique parties
 # - the minimum of candidatevotes and totalvotes are negative numbers, meaning that there are actually missing values
+
+# %% [markdown]
+# First we check if the triple <`year`, `state`, `congressional_district`> uniquely identifies each row:
+
+# %%
+elections_data.groupby(['year', 'state', 'congressional_district']).size().max() == 1
+
+# %% [markdown]
+# Then, we check if `candidatevotes` are always less or equal than `totalvotes`:
+
+# %%
+elections_data[elections_data['candidatevotes'] <= elections_data['totalvotes']].size == elections_data.size
+
+# %% [markdown]
+# We list the unique values in the column `state`:
+
+# %%
+states = elections_data['state'].unique()
+states.sort()
+print(f'States: {states}')
+print(f'Number of states: {states.size}')
+
+# %% [markdown]
+# All the states (District og Columbia included) are present.
+# 
+# We now display the states and the years for which there are missing rows:
+
+# %%
+years = [i for i in range(elections_data['year'].min(), elections_data['year'].max(), 2)]
+for year in years:
+    for state in states:
+        if elections_data[(elections_data['state']==state) & (elections_data['year']==year)].size == 0:
+            print(f"No data for '{state}' in {year}")
+
+# %% [markdown]
+# Except for District of Columbia, there are no missing rows.
+# For District of Columbia we have only the following row:
+
+# %%
+elections_data[elections_data['state']=='DISTRICT OF COLUMBIA']
+
+# %% [markdown]
+# Missing values are probably due to the fact that District of Columbia is a non voting delegate district. We found in [Wikipedia](https://en.wikipedia.org/wiki/2020_United_States_House_of_Representatives_election_in_the_District_of_Columbia) that while the number of votes received by the winning party coincides, the number of totalvotes is different. TODO: capire perchè e leggere il csv con i dati di Wiki.
+
+# %% [markdown]
+# We now check if congressional districts are numbered correctly (with '0' for states with only one congressional district, or with incremental values starting from '1' otherwise):
+
+# %%
+correct_numbering = True
+for state in states:
+    if state == 'DISTRICT OF COLUMBIA':
+        continue
+    for year in years:
+        districts = elections_data[(elections_data['state']==state) & (elections_data['year']==year)]['congressional_district'].unique()
+        districts.sort()
+        if districts.size > 1:
+            if (districts != [i for i in range(1, districts.size+1)]).any():
+                correct_numbering = False
+                break
+        elif districts[0] != 0:
+            correct_numbering = False
+            break
+correct_numbering
+
+# %% [markdown]
+# We now plot the distribution of `totalvotes` for each state in the years of interest, excluding 0 and negative values (this plot makes sense because congressional districts are redrawn so that the population of each district is roughly equal):
+
+# %%
+elections_data[
+    (elections_data['totalvotes']>0)&(elections_data['year']>2012)
+].boxplot(column='totalvotes', by='state', figsize=(20, 10), rot=90, xlabel='State', ylabel='Total votes')
+plt.suptitle('Total votes from 2014')
+plt.title('')
+plt.tight_layout()
+
+# %% [markdown]
+# We can observe that for both total and candidate votes Florida, Louisian and Oklahoma have lower outliers, while Maine has an upper outlier. 
+# 
+# We display the rows relative to Maine:
+
+# %%
+elections_data[(elections_data['year']>2013) & (elections_data['state']=='MAINE')]
+
+# %% [markdown]
+# We found in [Wikipedia](https://en.wikipedia.org/wiki/2022_United_States_House_of_Representatives_elections_in_Maine) that in Maine, that year, the Democratic party received 165136 votes out of a total of 311278 votes. We correct the error:
+
+# %%
+elections_data.at[10186, 'candidatevotes'] = 165136
+elections_data.at[10186, 'totalvotes'] = 311278
+
+# %%
+elections_data[
+    (elections_data['year']>2013) &
+    (elections_data['state'].isin(['FLORIDA', 'LOUSIANA', 'OKLAHOMA'])) &
+    ((elections_data['candidatevotes']<100) | (elections_data['totalvotes']<100))
+]
+
+# %% [markdown]
+# We found in Wikipedia (e.g. [here](https://en.wikipedia.org/wiki/2014_United_States_House_of_Representatives_elections_in_Florida)), that for all the years and states above, no candidates filed to challenge the incumbent representative for their seat. Therefore, we will copy the `candidatevotes` and `totalvotes` values from the previous year:
+
+# %%
+for index, row in elections_data.iterrows():
+    if row['candidatevotes'] < 2:
+        replacement = elections_data[
+            (elections_data['year']==row['year']-2) &
+            (elections_data['state']==row['state']) &
+            (elections_data['congressional_district']==row['congressional_district'])
+        ]
+        if replacement.size > 0:
+            elections_data.at[index, 'candidatevotes'] = replacement['candidatevotes'].iloc[0]
+            elections_data.at[index, 'totalvotes'] = replacement['totalvotes'].iloc[0]
+
+# %% [markdown]
+# We plot again the distribution of `totalvotes` and `candidatevotes` after cleaning the data:
+
+# %%
+elections_data[
+    (elections_data['year']>2012)
+].boxplot(column='totalvotes', by='state', figsize=(20, 10), rot=90, xlabel='State', ylabel='Total votes')
+plt.suptitle('Total votes from 2014')
+plt.title('')
+plt.tight_layout()
+
+# %% [markdown]
+# TODO: commentare
+
+# %% [markdown]
+# We get the unique names of the parties for the years of interest:
+
+# %%
+elections_data[
+    (elections_data['year']>2012)
+]['party'].unique()
+
+# %% [markdown]
+# The Democratic Farmer Labor is the affiliate of the Democratic Party in the U.S. state of Minnesota [[Wikipedia](https://en.wikipedia.org/wiki/Minnesota_Democratic–Farmer–Labor_Party)], hence we replace this party name with 'DEMOCRATIC' to ease later analysis.
+
+# %%
+elections_data['party'] = elections_data['party'].apply(
+    lambda x: 'DEMOCRATIC' if x=='DEMOCRATIC-FARMER-LABOR' else x
+)
+
+# %% [markdown]
+# TODO:
+# plot popolazione totale dello stato dal 2010 al 2022
+# plot di cose stacked: per ogni stato una colonna blu-rossa per 14, 16, 18? (aggregato per stato...)
+
+# %% [markdown]
+# We check if `candidatevotes` are always more than 50% of `totalvotes`:
+
+# %%
+elections_data[elections_data['candidatevotes'] <= 0.5 * elections_data['totalvotes']].size == elections_data.size
 
 
--- plot_utils.py
+++ plot_utils.py
@@ -1,7 +1,165 @@
+import geopandas
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
 from sklearn.inspection import DecisionBoundaryDisplay
+
+usa_code = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'District of Columbia': 'DC',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY'
+}
+
+def plot_usa_map(
+    df,
+    col_to_plot,
+    ax,
+    state_col='state',
+    vmin=None,
+    vmax=None,
+    title=None,
+    cbar_title=None,
+    cmap='coolwarm',
+    borders_path="./cb_2018_us_state_500k"
+    ):
+    geo_usa = geopandas.read_file(borders_path)
+    geo_merge=geo_usa.merge(df, left_on='NAME', right_on=state_col)
+    
+    #fig, continental_ax = plt.subplots(figsize=(20, 10))
+    alaska_ax = ax.inset_axes([-128,22,16,8], transform=ax.transData)
+    hawaii_ax = ax.inset_axes([-110,22.8,8,5], transform=ax.transData)
+
+    ax.set_xlim(-130, -64)
+    ax.set_ylim(22, 53)
+
+    alaska_ax.set_ylim(51, 72)
+    alaska_ax.set_xlim(-180, -127)
+
+    hawaii_ax.set_ylim(18.8, 22.5)
+    hawaii_ax.set_xlim(-160, -154.6)
+
+    if vmin==None or vmax==None:
+        vmin, vmax = df[col_to_plot].agg(['min', 'max']) # share the same colorbar
+    geo_merge.plot(column=col_to_plot, ax=ax, vmin=vmin, vmax=vmax, cmap=cmap,
+                   legend=True, legend_kwds={"label": cbar_title, "shrink": 0.5})
+    geo_merge.plot(column=col_to_plot, ax=alaska_ax, vmin=vmin, vmax=vmax, cmap=cmap)
+    geo_merge.plot(column=col_to_plot, ax=hawaii_ax, vmin=vmin, vmax=vmax, cmap=cmap)
+
+    for _, row in geo_merge.iterrows():
+        x = row['geometry'].centroid.coords[0][0]
+        y = row['geometry'].centroid.coords[0][1]
+        x_displacement = 0
+        y_displacement = 0
+        xytext = None
+        arrows = None
+        if row['NAME']=="Alaska":
+            x = -150
+            y = 65
+            xytext=(x,y)
+        elif row['NAME']=="Hawaii":
+            x = -157
+            y = 20.5
+            xytext=(x,y)
+        elif row['NAME']=="Maryland":
+            xytext = (x+4.5, y+0.5)
+            arrows = dict(arrowstyle="-")
+        elif row['NAME']=="District of Columbia":
+            xytext = (x+4.5, y-1)
+            arrows = dict(arrowstyle="-")
+        elif row['NAME']=="Delaware":
+            xytext =  (x+4.5, y+0.05)
+            arrows = dict(arrowstyle="-")
+        elif row['NAME']=="Rhode Island":
+            xytext =  (x+5, y-0.1)
+            arrows = dict(arrowstyle="-")
+        elif row['NAME']=="Connecticut":
+            xytext =  (x+4, y-1.5)
+            arrows = dict(arrowstyle="-")
+        elif row['NAME'] in ['Mississippi', 'West Virginia', 'New Hampshire']:
+            y_displacement = -0.35
+
+        alaska_ax.annotate(
+            text=row['NAME'],
+            xy=(x+x_displacement, y+y_displacement),
+            xytext=xytext,
+            arrowprops=arrows,
+            ha='center',
+            fontsize=8
+        )
+        hawaii_ax.annotate(
+            text=row['NAME'],
+            xy=(x+x_displacement, y+y_displacement),
+            xytext=xytext,
+            arrowprops=arrows,
+            ha='center',
+            fontsize=8
+        )
+        ax.annotate(
+            text=row['NAME'],
+            xy=(x+x_displacement, y+y_displacement),
+            xytext=xytext,
+            arrowprops=arrows,
+            ha='center',
+            fontsize=8
+        )
+    
+    ax.set_title(title, fontsize=16)
+
+    for ax in [ax, alaska_ax, hawaii_ax]:
+        ax.set_yticks([])
+        ax.set_xticks([])
+    
+    #return ax
+    #fig.show()
+    #fig.tight_layout()
 
 
 def map_plotly_plot(incidents_data, attribute):
