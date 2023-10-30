662a32751adea7f4093d946d101d901f1a53b596
iretes <i.testa@studenti.unipi.it>
2023-10-30 18:47:40 +0100

    indicators update

--- indicator_1.py
+++ indicator_1.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # %%
 import pandas as pd
 import numpy as np
 from plot_utils import hist_box_plot
 import matplotlib.pyplot as plt
-# %matplotlib inline
+%matplotlib inline
 
 # %%
 incidents_df = pd.read_csv('./data/final.csv', index_col=0)
@@ -12,6 +11,9 @@ incidents_df['date'] = pd.to_datetime(incidents_df['date'], format='%Y-%m-%d')
 
 # %%
 incidents_df['semester'] = (incidents_df['date'].dt.month // 7) + 1
+
+# %%
+incidents_df['city'] = incidents_df['city'].fillna('nan') # to treat all points without city as belonging to a fake city
 
 # %%
 def compute_ratio_indicator(df, gby, num, den, suffix, agg_fun):
@@ -34,7 +36,9 @@ incidents_df = compute_ratio_indicator(incidents_df, ['year', 'state', 'city'], 
 incidents_df = compute_ratio_indicator(incidents_df, ['year', 'state', 'city'], 'n_females', 'n_females', '_tot_year_city', 'sum')
 incidents_df = compute_ratio_indicator(incidents_df, ['year', 'state', 'city'], 'n_males', 'n_participants', '_year_city', 'sum')
 incidents_df = compute_ratio_indicator(incidents_df, ['year', 'state', 'city'], 'n_males', 'n_males', '_mean_year_city', 'mean')
+incidents_df = compute_ratio_indicator(incidents_df, ['year', 'state', 'city'], 'n_males', 'n_males', '_median_year_city', 'median')
 incidents_df = compute_ratio_indicator(incidents_df, ['year', 'state', 'city'], 'n_females', 'n_females', '_mean_year_city', 'mean')
+incidents_df = compute_ratio_indicator(incidents_df, ['year', 'state', 'city'], 'n_females', 'n_females', '_median_year_city', 'median')
 incidents_df = compute_ratio_indicator(incidents_df, ['year', 'semester', 'state'], 'n_males', 'n_males', '_tot_semester_state', 'sum')
 incidents_df = compute_ratio_indicator(incidents_df, ['year', 'semester', 'state'], 'n_females', 'n_females', '_tot_semester_state', 'sum')
 incidents_df = compute_ratio_indicator(incidents_df, ['year', 'semester', 'state'], 'n_males', 'n_participants', '_semester_state', 'sum')
@@ -52,31 +56,34 @@ incidents_df = compute_ratio_indicator(incidents_df, ['year', 'semester', 'state
 incidents_df.columns
 
 # %%
-ratio_wrt_tot = []
-ratio_wrt_mean = []
+ratios_wrt_tot = []
+ratios_wrt_mean = []
+ratios_wrt_median = []
 for att in incidents_df.columns:
     if 'ratio' in att:
-        if 'mean' not in att:
-            ratio_wrt_tot.append(att)
+        if 'mean' in att:
+            ratios_wrt_mean.append(att)
+        elif 'median' in att:
+            ratios_wrt_median.append(att)
         else:
-            ratio_wrt_mean.append(att)
+            ratios_wrt_tot.append(att)
 
 # %%
-incidents_df[ratio_wrt_tot].describe() # females quantiles are 0 (that's why they suggested to do it for males only)
+incidents_df[ratios_wrt_tot].describe() # females quantiles are 0 (that's why they suggested to do it for males only)
 
 # %%
-incidents_df[ratio_wrt_mean].describe()
+incidents_df[ratios_wrt_mean].describe()
 
 # %%
 incidents_df.boxplot(
-    column=ratio_wrt_tot,
+    column=ratios_wrt_tot,
     rot=90,
     figsize=(20, 10)
 ) # city has the higher box (that's why they suggested to do it for cities only)
 
 # %%
-log_ratio_wrt_tot = ['log_'+col for col in ratio_wrt_tot]
-incidents_df[log_ratio_wrt_tot] = np.log(incidents_df[ratio_wrt_tot]+0.000001)
+log_ratio_wrt_tot = ['log_'+col for col in ratios_wrt_tot]
+incidents_df[log_ratio_wrt_tot] = np.log(incidents_df[ratios_wrt_tot]+0.000001)
 incidents_df.boxplot(
     column=log_ratio_wrt_tot,
     rot=90,
@@ -84,8 +91,8 @@ incidents_df.boxplot(
 )
 
 # %%
-sqrt_ratio_wrt_tot = ['sqrt_'+col for col in ratio_wrt_tot]
-incidents_df[sqrt_ratio_wrt_tot] = np.sqrt(incidents_df[ratio_wrt_tot])
+sqrt_ratio_wrt_tot = ['sqrt_'+col for col in ratios_wrt_tot]
+incidents_df[sqrt_ratio_wrt_tot] = np.sqrt(incidents_df[ratios_wrt_tot])
 incidents_df.boxplot(
     column=sqrt_ratio_wrt_tot,
     rot=90,
@@ -94,12 +101,18 @@ incidents_df.boxplot(
 
 # %%
 incidents_df.boxplot(
-    column=ratio_wrt_mean,
+    column=ratios_wrt_mean,
     rot=90
 )
 
 # %%
-incidents_df[ratio_wrt_mean].describe()
+incidents_df.boxplot(
+    column=ratios_wrt_median,
+    rot=90
+)
+
+# %%
+incidents_df[ratios_wrt_mean].describe()
 
 # %%
 hist_box_plot(
@@ -111,36 +124,44 @@ hist_box_plot(
 )
 
 # %%
+incidents_df = compute_ratio_indicator(incidents_df, ['year', 'state'], 'n_participants_child', 'n_participants_child', '_tot_year_state', 'sum')
+incidents_df = compute_ratio_indicator(incidents_df, ['year', 'state'], 'n_participants_teen', 'n_participants_teen', '_tot_year_state', 'sum')
+incidents_df = compute_ratio_indicator(incidents_df, ['year', 'state'], 'n_participants_adult', 'n_participants_adult', '_tot_year_state', 'sum')
+incidents_df = compute_ratio_indicator(incidents_df, ['year', 'state'], 'n_participants_adult', 'n_participants', '_tot_year_state', 'sum')
+
+# %%
+incidents_df.boxplot(
+    column=[
+        'n_participants_child_n_participants_child_tot_year_state_ratio',
+        'n_participants_teen_n_participants_teen_tot_year_state_ratio',
+        'n_participants_adult_n_participants_adult_tot_year_state_ratio',
+        'n_participants_adult_n_participants_tot_year_state_ratio'
+        ],
+    rot=90
+)
+
+# %%
 population_df = pd.read_csv('./data/census_bureau.csv')
 population_df = population_df[population_df['congressional_district'] != 'ZZ'] # delete rows with obsolete congressional district
 population_df['congressional_district'] = population_df['congressional_district'].astype(int)
 population_df.loc[population_df['state'] == 'District of Columbia', 'congressional_district'] = 0 # to use the same notion of other dataframes
 population_df['state'] = population_df['state'].str.upper()
+population_df['n_males'] = population_df['male_child'] + population_df['male_teen'] + population_df['male_adult']
+population_df['n_females'] = population_df['female_child'] + population_df['female_teen'] + population_df['female_adult']
 population_df
 
-# %%
-population_per_year_state_by_gender = population_df.groupby(['year', 'state'])[['male_child', 'male_teen', 'male_adult', 'female_child', 'female_teen', 'female_adult']].sum()
-population_per_year_state_by_gender['n_males'] = population_per_year_state_by_gender['male_child'] + population_per_year_state_by_gender['male_teen'] + population_per_year_state_by_gender['male_adult']
-population_per_year_state_by_gender['n_females'] = population_per_year_state_by_gender['female_child'] + population_per_year_state_by_gender['female_teen'] + population_per_year_state_by_gender['female_adult']
-population_per_year_state_by_gender = population_per_year_state_by_gender[['n_males', 'n_females']]
-population_per_year_state_by_gender
 
 # %%
-incidents_df = incidents_df.merge(population_per_year_state_by_gender, on=['year', 'state'], how='left', suffixes=[None, '_pop_year_state'])
-incidents_df['n_males_pop_year_state_ratio'] = incidents_df['n_males'] / incidents_df['n_males_pop_year_state']
-incidents_df['n_females_pop_year_state_ratio'] = incidents_df['n_females'] / incidents_df['n_females_pop_year_state']
+def compute_ratio_external_indicator(df, ext_df, gby, num, den, suffix, agg_fun):
+    grouped_df = ext_df.groupby(gby)[den].agg(agg_fun)
+    df = df.merge(grouped_df, on=gby, how='left', suffixes=[None, suffix])
+    df[num+'_'+den+suffix+'_ratio'] = df[num] / df[den+suffix]
+    df.drop(columns=[den+suffix], inplace=True)
+    return df
 
 # %%
-population_per_year_congdist_by_gender = population_df.groupby(['year', 'state', 'congressional_district'])[['male_child', 'male_teen', 'male_adult', 'female_child', 'female_teen', 'female_adult']].sum()
-population_per_year_congdist_by_gender['n_males'] = population_per_year_congdist_by_gender['male_child'] + population_per_year_congdist_by_gender['male_teen'] + population_per_year_congdist_by_gender['male_adult']
-population_per_year_congdist_by_gender['n_females'] = population_per_year_congdist_by_gender['female_child'] + population_per_year_congdist_by_gender['female_teen'] + population_per_year_congdist_by_gender['female_adult']
-population_per_year_congdist_by_gender = population_per_year_congdist_by_gender[['n_males', 'n_females']]
-population_per_year_congdist_by_gender
-
-# %%
-incidents_df = incidents_df.merge(population_per_year_congdist_by_gender, on=['year', 'state'], how='left', suffixes=[None, '_pop_year_congdist'])
-incidents_df['n_males_pop_year_congdist_ratio'] = incidents_df['n_males'] / incidents_df['n_males_pop_year_congdist']
-incidents_df['n_females_pop_year_congdist_ratio'] = incidents_df['n_females'] / incidents_df['n_females_pop_year_congdist']
+incidents_df = compute_ratio_external_indicator(incidents_df, population_df, ['year', 'state'], 'n_males', 'n_males', '_pop_year_state', 'sum')
+incidents_df = compute_ratio_external_indicator(incidents_df, population_df, ['year', 'state', 'congressional_district'], 'n_males', 'n_males', '_pop_year_cong', 'sum')
 
 # %%
 pop_ratios = []
@@ -155,12 +176,7 @@ incidents_df.boxplot(
 
 # %%
 # TODO:
-# fare funzione anche per popolazione esterna e studiare distribuzioni
-# fare gli stessi indici con child, teen, adult
-# considerare i tag
-# usare popolazione esterna per fare statistiche aggregate (indici a livello di stato, distretto congressuale - anno - semestre, non per record)
-# aggregazione più fine-grossa sul tempo (non credo sia necessario)
-# combinare genere e fasce di età (?)
-# usare dati di città e contee da census?
+# considerare i tag, età, povertà
+# entropia?
 
 
