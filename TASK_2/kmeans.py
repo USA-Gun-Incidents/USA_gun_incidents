8e6e9917e649e9de6c33ceb424234f6be2922559
iretes <i.testa@studenti.unipi.it>
2023-11-14 22:37:55 +0100

    pca outliers, corr matrix, scores w.r.t categories

--- TASK_2/kmeans.py
+++ TASK_2/kmeans.py
@@ -10,8 +10,11 @@ import plotly.express as px
 import warnings
 np.warnings = warnings # altrimenti numpy da problemi con pyclustering, TODO: è un problema solo mio?
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
-from sklearn.metrics import davies_bouldin_score, calinski_harabasz_score, silhouette_score
+from sklearn.metrics import davies_bouldin_score, calinski_harabasz_score, silhouette_score, adjusted_rand_score
+from sklearn.metrics import homogeneity_score, completeness_score, normalized_mutual_info_score
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, roc_auc_score
 from sklearn.cluster import KMeans, BisectingKMeans
+from scipy.spatial.distance import pdist, squareform
 from pyclustering.cluster.xmeans import xmeans, splitting_type
 from pyclustering.cluster.center_initializer import kmeans_plusplus_initializer
 from yellowbrick.cluster import KElbowVisualizer, SilhouetteVisualizer, InterclusterDistance
@@ -167,7 +170,7 @@ for k in best_k:
 
 # %%
 results_df = pd.DataFrame(results).T
-results_df.drop(columns=['model']) # only for printing
+results_df.drop(columns=['model'])
 
 # %%
 k = 5
@@ -311,6 +314,36 @@ pca = PCA()
 X_pca = pca.fit_transform(incidents_df[features_to_cluster])
 
 # %%
+exp_var_pca = pca.explained_variance_ratio_
+plt.bar(range(0,len(exp_var_pca)), exp_var_pca, align='center')
+plt.ylabel('Explained variance ratio')
+plt.xlabel('Principal component')
+plt.title('Explained variance by principal component')
+plt.xticks(np.arange(0,len(exp_var_pca),1.0));
+
+# %%
+pca_labels = {
+    str(i): f"PC {i+1} ({var:.1f}%)"
+    for i, var in enumerate(pca.explained_variance_ratio_ * 100)
+}
+
+cmap = ['rgb'+str(sns.color_palette()[c%6]) for c in clusters_ids]
+
+fig = px.scatter_matrix(
+    X_pca,
+    labels=pca_labels,
+    dimensions=range(5),
+    color_discrete_sequence=cmap,
+    color=labels.astype(str),
+    height=800
+)
+fig.update_traces(diagonal_visible=False, showupperhalf=False)
+fig.update_layout(
+        legend_title_text="Cluster"
+    )
+fig.show()
+
+# %%
 plt.scatter(X_pca[:, 0], X_pca[:, 1], edgecolor='k', s=40, c=colors)
 plt.xlabel('Component 1')
 plt.ylabel('Component 2')
@@ -318,10 +351,39 @@ plt.title('PCA')
 plt.legend(handles=legend_elements)
 
 # %%
-silhouette_vis = SilhouetteVisualizer(kmeans, title='Silhouette plot') # FIXME: use same color
+silhouette_vis = SilhouetteVisualizer(kmeans, title='Silhouette plot', colors=sns.color_palette().as_hex())
 silhouette_vis.fit(X)
+silhouette_scores = silhouette_vis.silhouette_samples_
 silhouette_vis.show()
-# TODO: colorare negli scatter i punti con silhouette negativa?
+
+# %%
+new_labels = np.full(labels.shape[0], -1)
+for i, s in enumerate(silhouette_scores):
+    if s >= 0:
+        new_labels[i] = labels[i]
+
+unique_lbl, idx = np.unique(new_labels, return_index=True)
+unique_lbl = unique_lbl[np.argsort(idx)]
+cmap=[]
+for i, lbl in enumerate(unique_lbl):
+    if lbl != -1:
+        cmap.append('rgb'+str(sns.color_palette()[lbl]))
+    else:
+        cmap.append('rgb(0,0,0)')
+
+fig = px.scatter_matrix(
+    X_pca,
+    labels=pca_labels,
+    dimensions=range(5),
+    color_discrete_sequence=cmap,
+    color=new_labels.astype(str),
+    height=800
+)
+fig.update_traces(diagonal_visible=False, showupperhalf=False)
+fig.update_layout(
+        legend_title_text="Cluster"
+    )
+fig.show()
 
 # %%
 visualizer = InterclusterDistance(kmeans)
@@ -329,7 +391,35 @@ visualizer.fit(X)
 visualizer.show()
 
 # %%
-# TODO: similarity matrix (need to do it on a subsample)
+# TODO: assicurasi sia corretto...
+subsampled_incidents_df = incidents_df.groupby('cluster', group_keys=False).apply(lambda x: x.sample(frac=0.05, random_state=0))
+subsampled_incidents_df.reset_index(inplace=True)
+subsampled_incidents_df.sort_values(by=['cluster'], inplace=True)
+
+dm = squareform(pdist(X[subsampled_incidents_df.index]))
+
+n_subsampled_points = subsampled_incidents_df.shape[0]
+im = np.zeros((n_subsampled_points, n_subsampled_points))
+sub_labels = subsampled_incidents_df['cluster'].values
+for i in range(n_subsampled_points):
+    for j in range(n_subsampled_points):
+        if sub_labels[i] == sub_labels[j]:
+            im[i, j] = 1
+
+corr_matrix = np.corrcoef(dm, im)
+
+plt.matshow(corr_matrix)
+
+# %%
+plt.matshow(im)
+
+# %%
+plt.matshow(dm)
+
+# %%
+corr_matrix = np.corrcoef(dm, im, rowvar=False)
+
+plt.matshow(corr_matrix)
 
 # %%
 ncols = 3
@@ -348,15 +438,79 @@ for ax in axs[nrows-1, id%ncols:]:
 # %%
 for feature in features_to_cluster:
     axes = incidents_df[feature].hist(by=incidents_df['cluster'], bins=20, layout=(1,k), figsize=(20, 5))
-    plt.suptitle(f'Distribution of {attr} in each cluster', fontweight='bold')
+    plt.suptitle(f'Distribution of {feature} in each cluster', fontweight='bold')
     for i, ax in enumerate(axes):
         ax.set_title(f'Cluster {i}')
         ax.set_xlabel(feature)
         ax.set_ylabel('Number of incidents')
 
 # %%
-# metti altrove
-# guarda indicatori
-# guarda clustering altri
+cat_metrics_df = pd.DataFrame()
+
+adj_rand_scores = []
+homogeneity_scores = []
+completeness_scores = []
+mutual_info_scores = []
+
+for column in incidents_df[categorical_features].columns: # all permutation invariant
+    adj_rand_scores.append(adjusted_rand_score(incidents_df[column], incidents_df['cluster']))
+    mutual_info_scores.append(normalized_mutual_info_score(incidents_df[column], incidents_df['cluster'], average_method='arithmetic'))
+    homogeneity_scores.append(homogeneity_score(incidents_df[column], incidents_df['cluster']))
+    completeness_scores.append(completeness_score(incidents_df[column], incidents_df['cluster']))
+
+cat_metrics_df['feature'] = categorical_features
+cat_metrics_df['adjusted rand score'] = adj_rand_scores
+cat_metrics_df['normalized mutual information'] = mutual_info_scores
+cat_metrics_df['homogeneity'] = homogeneity_scores
+cat_metrics_df['completeness'] = completeness_scores
+
+
+cat_metrics_df.set_index(['feature'], inplace=True)
+cat_metrics_df
+
+# %%
+from sklearn.metrics import confusion_matrix
+
+def align_labels(label1, label2):
+    cm = confusion_matrix(label1, label2)
+    cm_argmax = cm.argmax(axis=0)
+    label2 = np.array([cm_argmax[i] for i in label2])
+    return label1, label2
+
+# %%
+label1 = [0,0,0,1,1,1,2,2,2]
+label2 = [1,1,1,0,0,2,2,2,2]
+
+label1, label2 = align_labels(label1, label2)
+label2
+
+# %%
+confusion_matrix(label1, label2)
+
+# %%
+cat_clf_metrics_df = pd.DataFrame()
+
+accuracy = []
+f1 = []
+precision = []
+recall = []
+#roc_auc = []
+# TODO: entropy and purity
+
+for column in incidents_df[categorical_features[:-1]].columns: # FIXME: -1 to avoid exception
+    _, cluster_labels = align_labels(incidents_df[column], incidents_df['cluster'])
+    accuracy.append(accuracy_score(incidents_df[column], cluster_labels))
+    f1.append(f1_score(incidents_df[column], cluster_labels, average='weighted'))
+    precision.append(precision_score(incidents_df[column], cluster_labels, average='weighted', zero_division=0))
+    recall.append(recall_score(incidents_df[column], cluster_labels, average='weighted', zero_division=0))
+    #roc_auc.append(roc_auc_score(incidents_df[column], cluster_labels, multi_class='ovr', average='weighted')) # FIXME: not working
+
+cat_clf_metrics_df['feature'] = categorical_features[:-1]
+cat_clf_metrics_df['accuracy'] = accuracy
+cat_clf_metrics_df['f1'] = f1
+cat_clf_metrics_df['precision'] = precision
+cat_clf_metrics_df['recall'] = recall
+cat_clf_metrics_df.set_index(['feature'], inplace=True)
+cat_clf_metrics_df
 
 
