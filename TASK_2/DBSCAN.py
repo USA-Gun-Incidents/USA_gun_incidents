979eaa69e5073289ca5f388dddb6596fed2d3e40
Giu <giuliaghisolfi99@gmail.com>
2023-11-26 10:45:50 +0100

    DBSCAN final parametrs

--- TASK_2/DBSCAN.py
+++ TASK_2/DBSCAN.py
@@ -15,6 +15,10 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.cluster import DBSCAN
+from sklearn import metrics 
+from scipy.spatial.distance import pdist, squareform
 from plot_utils import plot_scattermap_plotly
 
 # %%
@@ -31,7 +35,7 @@ incidents_df.head(2)
 # Prepare dataset and a list of indices and numerical attributes for choosen state: Illinois.
 
 # %%
-illinois_df = incidents_df[incidents_df['state']=='ILLINOIS'].dropna()
+illinois_df = incidents_df[incidents_df['state']=='ILLINOIS']
 illinois_df.head(2)
 
 # %%
@@ -90,12 +94,11 @@ plt.show()
 # %%
 ind_names_list = [
     # geo
-    'location_imp',
     'surprisal_address_type',
     # age
     'avg_age',
     # participants
-    'severity',
+    'n_killed',
     'n_males_prop',
     'n_arrested_prop',
     'n_participants',
@@ -114,6 +117,7 @@ plt.show()
 # %%
 illinois_df[ind_names_list].describe()
 
+
 # %% [markdown]
 # We observe that the selected features exhibit low correlation among themselves and display distinct value ranges.
 
@@ -121,19 +125,40 @@ illinois_df[ind_names_list].describe()
 # ## Utilities
 
 # %% [markdown]
-# in this sedtion we prepare some usefull function in order to perform the clustering analisys
+# In this section, we have prepared several useful functions for conducting clustering analysis. Here is a brief documentation for each function:
 #
-# Here a brief documentation:
+# 1. **Function: standardization**
+#    - Description: Standardizes data in the input dataframe (*df*) within the specified columns using either the 'Z-score' or 'MinMax' standardization methods. Both methods are implemented using objects imported from the sklearn library.
+#    - Input:
+#      - *df*: Input dataframe containing columns to be standardized.
+#      - *columns*: List of column names to be standardized.
+#      - *method*: The method of standardization to be used, either 'Z-score' or 'MinMax'.
+#    - Output:
+#      - A *np.ndarray* with the specified columns standardized.
 #
-# function *standardization*: standardize data in in the dataframe given in input contenuti nelle colonne columns, usando metodo 'Z-score' or 'MinMax' standardization, both using object import from sklearn
+# 2. **Function: find_best_eps**
+#    - Description: Implements the knee method to select the best candidates as values of *eps* for all the *min_samples* tried. Euclidean distance is used for this selection.
+#    - Input:
+#      - *X*: Input data array.
+#      - *min_samples_range*: Range of values for *min_samples* parameter.
+#    - Output:
+#      - Plot the graph of distance from k-th nearest neighbour
 #
-# function *plot_dbscan*: this function visualize the results of the clustering analisys via DBSCAN, 
-# plotta i punti appartenenti ai cluster in colori diversi,
-# questa funzione fornisce una rappresentazione visuale del risultato del clustering effettuato da DBSCAN sui dati forniti.
+# 3. **Function: dbscan**
+#    - Description: Performs the DBSCAN algorithm using the **DBSCAN** library from sklearn.cluster. The function returns a dictionary containing *eps* and *min_samples* given as input, the estimated number of noise points, the estimated number of clusters, and the silhouette coefficient. This function is used to select the best parameters (*eps* and *min_samples*).
+#    - Input:
+#      - *X*: Input data array.
+#      - *eps*: Maximum distance between two samples for them to be considered in the same neighborhood.
+#      - *min_samples*: Minimum number of points needed to create a cluster.
+#    - Output:
+#      - A dictionary containing information about the clustering results.
+#
+# We have chosen to use the silhouette coefficient as a measure to evaluate the algorithm's performance. The silhouette coefficient ranges from -1 to +1, where a higher value indicates that objects are well-matched to their own cluster and poorly matched to neighboring clusters.
+
+# %% [markdown]
+# ### Standardize Data
 
 # %%
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-
 def standardization(df, columns, standardizer='Zscore'):
     if standardizer == 'Zscore':
         standardizer = StandardScaler()
@@ -143,7 +168,58 @@ def standardization(df, columns, standardizer='Zscore'):
     scaler.fit(df[columns].values)
     return scaler.transform(df[columns].values)
 
+# %% [markdown]
+# ### Find best EPS
+
 # %%
+def find_best_eps(X, min_samples_range=[3, 5, 9, 15]):
+    dist = pdist(X, 'euclidean') # pair wise distance
+    dist = squareform(dist) # distance matrix given the vector dist
+    
+    # Calculate sorted list of distances for points for each k in k_list
+    # and plot the graph of distance from k-th nearest neighbour
+    fig, ax = plt.subplots(int(np.ceil(len(min_samples_range)/3)), 3, figsize=(20, 8))
+
+    for i, k in enumerate(min_samples_range):
+        kth_distances = list()
+        for d in dist:
+            index_kth_distance = np.argsort(d)[k]
+            kth_distances.append(d[index_kth_distance])
+
+        # Plot the graph of distance from k-th nearest neighbour
+        ax[int(i/3), int(i%3)].plot(range(0, len(kth_distances)), sorted(kth_distances))
+        ax[int(i/3), int(i%3)].set_ylabel('%sth near neighbor distance' %k)
+        ax[int(i/3), int(i%3)].set_xlabel('Point Sorted according to distance of %sth near neighbor' %k)
+        ax[int(i/3), int(i%3)].tick_params(axis='both', which='major', labelsize=8)
+        ax[int(i/3), int(i%3)].grid(linestyle='--', linewidth=0.5, alpha=0.6)
+
+    plt.show()
+
+
+# %% [markdown]
+# ### DBASCAN Algorithm
+
+# %%
+def dbscan(X, eps=0.1, min_samples=10):
+    # Compute DBSCAN      
+    db = DBSCAN(eps=eps, min_samples=min_samples).fit(X)
+    labels = db.labels_
+
+    # Number of clusters in labels, ignoring noise if present.
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+    n_noise_ = list(labels).count(-1)
+
+    return {'eps': eps, 'min_samples': min_samples, 
+        '#clusters': len(set(labels)) - (1 if -1 in labels else 0),
+        '#noise': list(labels).count(-1),  '%noise': list(labels).count(-1)/X.shape[0]*100,
+        'silhouette_coef': metrics.silhouette_score(X, labels), 
+        '#cluster0': list(labels).count(0), '#cluster1': list(labels).count(1), 
+        '#cluster2': list(labels).count(2), '#cluster3': list(labels).count(3), 
+        '#cluster4': list(labels).count(4), '#cluster5': list(labels).count(5),
+        '#cluster6': list(labels).count(6), '#cluster7': list(labels).count(7)}
+
+# %%
+#TODO: togliere o spostare in utils
 def plot_dbscan(X, db, columns): 
     labels = db.labels_ 
     n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)  
@@ -190,121 +266,17 @@ def plot_dbscan(X, db, columns):
     plt.title(f"Estimated number of clusters: {n_clusters_}")
     plt.show()
 
-# %%
-from sklearn.cluster import DBSCAN
-from sklearn import metrics 
-
-def dbscan(X, eps=0.1, min_samples=10, plot_clusters=False):
-    # Compute DBSCAN      
-    db = DBSCAN(eps=eps, min_samples=min_samples).fit(X)
-    labels = db.labels_
-
-    # Number of clusters in labels, ignoring noise if present.
-    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-    n_noise_ = list(labels).count(-1)
-
-    if plot_clusters:
-        print("Estimated number of clusters: %d" % n_clusters_)
-        print("Estimated number of noise points: %d" % n_noise_)
-        if len(np.unique(labels)) > 1:
-            print("Silhouette Coefficient: %0.3f"
-                % metrics.silhouette_score(X, labels))
-        plot_dbscan(X, db)
-    
-    return {'eps': eps, 'min_samples': min_samples, 
-        '#clusters': len(set(labels)) - (1 if -1 in labels else 0),
-        '#noise': list(labels).count(-1),  '%noise': list(labels).count(-1)/X.shape[0]*100,
-        'silhouette_coef': metrics.silhouette_score(X, labels), 
-        '#cluster0': list(labels).count(0), '#cluster1': list(labels).count(1), 
-        '#cluster2': list(labels).count(2), '#cluster3': list(labels).count(3), 
-        '#cluster4': list(labels).count(4), '#cluster5': list(labels).count(5),
-        '#cluster6': list(labels).count(6), '#cluster7': list(labels).count(7)}
-
-# %% [markdown]
-# The silhouette value is a measure of how similar an object is to its own cluster (cohesion) compared to other clusters (separation). The silhouette ranges from −1 to +1, where a high value indicates that the object is well matched to its own cluster and poorly matched to neighboring clusters. If most objects have a high value, then the clustering configuration is appropriate. If many points have a low or negative value, then the clustering configuration may have too many or too few clusters.
-
-# %% [markdown]
-# ### Find best EPS
-
-# %%
-from scipy.spatial.distance import pdist, squareform
-
-def find_best_eps(X, k_list=[3, 5, 9, 15]):
-    dist = pdist(X, 'euclidean') # pair wise distance
-    dist = squareform(dist) # distance matrix given the vector dist
-    
-    # Calculate sorted list of distances for points for each k in k_list
-    # and plot the graph of distance from k-th nearest neighbour
-    fig, ax = plt.subplots(int(np.ceil(len(k_list)/3)), 3, figsize=(20, 8))
-
-    for i, k in enumerate(k_list):
-        kth_distances = list()
-        for d in dist:
-            index_kth_distance = np.argsort(d)[k]
-            kth_distances.append(d[index_kth_distance])
-
-        # Plot the graph of distance from k-th nearest neighbour
-        ax[int(i/3), int(i%3)].plot(range(0, len(kth_distances)), sorted(kth_distances))
-        ax[int(i/3), int(i%3)].set_ylabel('%sth near neighbor distance' %k)
-        ax[int(i/3), int(i%3)].set_xlabel('Point Sorted according to distance of %sth near neighbor' %k)
-        #ax[int(i/3), int(i%3)].set_yticks(np.linspace(0, 5, 12))
-        #ax[int(i/3), int(i%3)].set_ylim(0, 3)
-        ax[int(i/3), int(i%3)].tick_params(axis='both', which='major', labelsize=8)
-        ax[int(i/3), int(i%3)].grid(linestyle='--', linewidth=0.5, alpha=0.6)
-
-    plt.show()
-
-# %% [markdown]
-# ## Clustering: Illinois
-
-# %%
-#kneed_algorithm(X_std_illinois, neighbors=5)
-# DBSCAN(eps=1.75, min_samples=5).fit(X_std_illinois) #dati stadardizzati, eps=1.75, min_samples=5, 5 clusters
-
-# %% [markdown]
-# ### MinMax Scale Data
-
-# %%
-X_minmax_illinois = (illinois_df[ind_names_list], columns=ind_names_list)
-
-# %%
-fig, ax = plt.subplots(figsize=(15, 5))
-plt.boxplot(X_std_illinois, vert=True, labels=ind_names_list)
-plt.xticks(rotation=90, ha='right')
-plt.show()
-
-# %%
-find_best_eps(X_minmax_illinois, k_list=[3, 5, 9, 15, 20, 30])
-
-# %%
-eps = [0.1, 0.15, 0.2, 0.25]
-# eps: maximum distance between two samples for one to be considered as in the neighborhood of the other.
-min_samples = [3, 5, 10, 15, 20]
-
-dbscan_illinois = pd.DataFrame(columns=['eps', 'min_samples', '#clusters', '#noise', '%noise', 'silhouette_coef',
-    '#cluster0', '#cluster1', '#cluster2', '#cluster3', '#cluster4', '#cluster5', '#cluster6', '#cluster7'])
-
-for e in eps:
-    for k in min_samples:
-        db = dbscan(X_minmax_illinois, eps=e, min_samples=k, plot_clusters=False)
-        dbscan_illinois = pd.concat([dbscan_illinois, pd.DataFrame(db, index=[0])], ignore_index=True)
-
-# %%
-dbscan_illinois
-
-# %% [markdown]
-# ### Visualize data
-
-# %%
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_dbscan_subplots(X, db):
+def plot_dbscan_subplots(X, db, columns=[]):
     labels = db.labels_
     n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
     unique_labels = set(labels)
     core_samples_mask = np.zeros_like(labels, dtype=bool)
     core_samples_mask[db.core_sample_indices_] = True
+    if len(columns) == 0:
+        columns = [f'Dimension {i}' for i in range(X.shape[1])]
 
     n_dimensions = X.shape[1]
 
@@ -319,7 +291,7 @@ def plot_dbscan_subplots(X, db):
             ax.set_yticks([])
 
             if i == j:
-                ax.text(0.5, 0.5, f'Dimension {i + 1}', ha='center', va='center', fontsize=10, color='black')
+                ax.text(0.5, 0.5, f'{columns[i]}', ha='center', va='center', fontsize=8, color='black')
             else:
                 for k, col in zip(unique_labels, colors):
                     if k == -1:
@@ -333,7 +305,7 @@ def plot_dbscan_subplots(X, db):
                         "o",
                         markerfacecolor=tuple(col),
                         markeredgecolor='k',
-                        markersize=10,
+                        markersize=4 if k == -1 else 6,
                         label=f'Cluster {k}'
                     )
 
@@ -344,21 +316,81 @@ def plot_dbscan_subplots(X, db):
                         "o",
                         markerfacecolor=tuple(col),
                         markeredgecolor=col,
-                        markersize=6,
+                        markersize=2 if k == -1 else 4,
                         label=f'Cluster {k}'
                     )
 
-    plt.subplots_adjust(hspace=0.5, wspace=0.5)
+                    ax.grid(linestyle='--', linewidth=0.5, alpha=0.6)
     plt.show()
 
-# Esempio di utilizzo
-# plot_dbscan_subplots(X, db)
+# %% [markdown]
+# ## DBSCAN Algorithm: Illinois
 
+# %% [markdown]
+# ### Prepare Data and Parameters selection
 
+# %% [markdown]
+# We prepare the data by selecting only the entries in the dataset corresponding to the Illinois state and where all the values corresponding to the selected attributes, on which the clustering algorithm operates, are not NaN. Since the data have different range values for the various attributes, we decided to standardize using the 'MinMax' scaler, which, from some preliminary trials, has proven to be the most effective.
+#
+# We also save all the attribute values for the entries we use in the dataframe *illinois_df*.
 
 # %%
-db = DBSCAN(eps=0.2, min_samples=10).fit(X_minmax_illinois) #12
-#plot_dbscan(X_std_illinois, db)
+X_minmax_illinois = standardization(
+    df=incidents_df[incidents_df['state']=='ILLINOIS'][ind_names_list].dropna(), 
+    columns=ind_names_list, standardizer='MinMax')
+
+# %%
+illinois_df = incidents_df.loc[incidents_df[incidents_df['state']=='ILLINOIS'][ind_names_list].dropna().index]
+
+# %%
+illinois_df.shape
+
+# %% [markdown]
+# We plot boxplots representing the various attributes of our standardized data, revealing the presence of numerous outliers.
+
+# %%
+fig, ax = plt.subplots(figsize=(15, 5))
+plt.boxplot(X_minmax_illinois, vert=True, labels=ind_names_list)
+plt.xticks(rotation=90, ha='right')
+plt.show()
+
+# %% [markdown]
+# We utilize the Kneed algorithm to identify the range for the optimal epsilon *eps*.
+
+# %%
+find_best_eps(X_minmax_illinois, min_samples_range=[3, 5, 9, 15, 20, 30])
+
+# %% [markdown]
+# Upon examining the plot, we decide to explore epsilon values in the range between 1.25 and 2.
+
+# %%
+eps = [1.25, 1.5, 1.75, 2]
+# eps: maximum distance between two samples for one to be considered as in the neighborhood of the other.
+min_samples = [6, 12, 20]
+
+dbscan_illinois = pd.DataFrame(columns=['eps', 'min_samples', '#clusters', '#noise', '%noise', 'silhouette_coef',
+    '#cluster0', '#cluster1', '#cluster2', '#cluster3', '#cluster4', '#cluster5', '#cluster6', '#cluster7'])
+
+for e in eps:
+    for k in min_samples:
+        db = dbscan(X_minmax_illinois, eps=e, min_samples=k)
+        dbscan_illinois = pd.concat([dbscan_illinois, pd.DataFrame(db, index=[0])], ignore_index=True)
+
+# %%
+dbscan_illinois
+
+# %% [markdown]
+# ### Perform Clustering
+
+# %% [markdown]
+# We selected *eps* = 1.5 and *min-samples* = 20
+
+# %%
+db = DBSCAN(eps=1.5, min_samples=20).fit(X_minmax_illinois) # 5 first trial
+plot_dbscan_subplots(X_minmax_illinois, db, columns=ind_names_list)
+
+# %% [markdown]
+# ## Results 
 
 # %%
 # bar plot of number of incidents per cluster
@@ -436,8 +468,8 @@ for i in range(8):
 
 # %%
 # plot hist for poverty_perc for each cluster
-fig, ax = plt.subplots(6, 2, figsize=(20, 10), sharex=True, sharey=True)
-for i in range(12):
+fig, ax = plt.subplots(3, 2, figsize=(20, 10), sharex=True, sharey=True)
+for i in range(6):
     ax[int(i/2), i%2].hist(illinois_df['poverty_perc'].values[db.labels_==i-1], 
         bins=int(1+3.3*np.log(illinois_df[illinois_df['cluster']==i-1].shape[0])), 
         label=f'Cluster {i-1}', edgecolor='black', linewidth=0.8, alpha=0.7,
@@ -451,8 +483,8 @@ for i in range(12):
 
 # %%
 # plot hist for congd for each cluster
-fig, ax = plt.subplots(6, 2, figsize=(20, 10), sharex=True, sharey=True)
-for i in range(12):
+fig, ax = plt.subplots(3, 2, figsize=(20, 10), sharex=True, sharey=True)
+for i in range(6):
     ax[int(i/2), i%2].hist(illinois_df['congd'].values[db.labels_==i-1], 
         bins=int(1+3.3*np.log(illinois_df[illinois_df['cluster']==i-1].shape[0])), 
         label=f'Cluster {i-1}', edgecolor='black', linewidth=0.8, alpha=0.7,
@@ -478,8 +510,8 @@ illinois_df.head(2)
 
 # %%
 for attribute in ['male_child', 'male_teen', 'male_adult', 'female_child', 'female_teen', 'female_adult']:
-    fig, ax = plt.subplots(4, 3, figsize=(20, 8), sharex=True, sharey=True)
-    for i in range(12):
+    fig, ax = plt.subplots(2, 3, figsize=(20, 8), sharex=True, sharey=True)
+    for i in range(6):
         ax[int(i/3), i%3].hist(illinois_df[attribute].values[db.labels_==i-1], 
             bins=int(1+3.3*np.log(illinois_df[illinois_df['cluster']==i-1].shape[0])), 
             label=f'Cluster {i-1}', edgecolor='black', linewidth=0.8, alpha=0.7,
@@ -505,3 +537,68 @@ plot_scattermap_plotly(illinois_df, 'cluster', zoom=5, title='Incidents clustere
 # %%
 plot_scattermap_plotly(illinois_df[illinois_df['county']=='Cook County'], 'cluster', 
     zoom=8, title='Incidents clustered by DBSCAN')
+
+# %% [markdown]
+# ## Proximity matrix
+
+# %%
+cluster_df = pd.DataFrame()
+cluster_df['DBS_labels'] = db.labels_
+cluster_df['data_point'] = range(len(cluster_df))
+cluster_df.index = pd.RangeIndex(len(cluster_df)) # define an index from 0 to (len(data))
+cluster_df = cluster_df.sort_values(by='DBS_labels')
+
+# %%
+from scipy.spatial import distance_matrix
+
+dist_matrix = distance_matrix(cluster_df, cluster_df)
+ax = sns.heatmap(dist_matrix)
+
+# %% [markdown]
+# ### compare with K-means
+
+# %%
+from sklearn.cluster import KMeans
+from clustering_utils import compute_bss_per_cluster
+from sklearn.metrics import davies_bouldin_score, calinski_harabasz_score, silhouette_score
+
+
+# %%
+def fit_kmeans(X, params):
+    print(f"Fitting KMeans with k={params['n_clusters']}")
+    kmeans = KMeans(**params)
+    kmeans.fit(X)
+    results = {}
+    results['model'] = kmeans
+    results['SSE'] = kmeans.inertia_
+    results['BSS'] = compute_bss_per_cluster(X=X, clusters=kmeans.labels_, centroids=kmeans.cluster_centers_, weighted=True).sum()
+    results['davies_bouldin_score'] = davies_bouldin_score(X=X, labels=kmeans.labels_)
+    results['calinski_harabasz_score'] = calinski_harabasz_score(X=X, labels=kmeans.labels_)
+    results['silhouette_score'] = silhouette_score(X=X, labels=kmeans.labels_) 
+    results['n_iter'] = kmeans.n_iter_
+    return results
+
+
+# %%
+MAX_ITER = 300
+N_INIT = 10
+INIT_METHOD = 'k-means++'
+MAX_K = 30
+RANDOM_STATE = 42
+
+# %%
+results = {}
+kmeans_params = {}
+kmeans_params['random_state'] = RANDOM_STATE
+kmeans_params['max_iter'] = MAX_ITER
+best_k = 6
+
+kmeans_params['n_init'] = N_INIT
+kmeans_params['n_clusters'] = k
+kmeans_params['init'] = INIT_METHOD
+result = fit_kmeans(X=X_minmax_illinois, params=kmeans_params)
+results[str(k)+'means'] = result
+
+# %%
+results_df = pd.DataFrame(results).T
+results_df.drop(columns=['model'])
