d1b81fc7233724d64570222b2a272174b549daa7
iretes <i.testa@studenti.unipi.it>
2023-12-21 19:37:54 +0100

    dt pipeline fix

--- TASK_3/dt.py
+++ TASK_3/dt.py
@@ -1,16 +1,19 @@
 # %%
 import pandas as pd
 import json
+import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import tree
 import pydotplus
 from IPython.display import Image
-from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
-from sklearn.metrics import classification_report, make_scorer, f1_score
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import make_scorer, f1_score
+from time import time
 from classification_utils import *
 pd.set_option('display.max_columns', None)
 pd.set_option('max_colwidth', None)
 RANDOM_STATE = 42
+RESULTS_DIR = '../data/classification_results'
 
 # %%
 # load the data
@@ -29,79 +32,65 @@ indicators_train_df = incidents_train_df[features_for_clf]
 indicators_test_df = incidents_test_df[features_for_clf]
 
 # %%
-dt = tree.DecisionTreeClassifier(
-    criterion='gini',
-    splitter='best',
-    max_depth=10,
-    min_samples_split=3,
-    min_samples_leaf=4
-)
-dt = dt.fit(indicators_train_df, true_labels_train_df)
-
-# %%
-dot_data = tree.export_graphviz(
-    dt,
-    out_file=None, 
-    feature_names=list(indicators_train_df.columns),
-    filled=True,
-    rounded=True
-)
-graph = pydotplus.graph_from_dot_data(dot_data)  
-Image(graph.create_png())
-
-# %%
-pred_labels_train = dt.predict(indicators_train_df)
-pred_probas_train = dt.predict_proba(indicators_train_df)
-pred_labels_test = dt.predict(indicators_test_df)
-pred_probas_test = dt.predict_proba(indicators_test_df)
-
-# %%
-print(classification_report(y_true=true_labels_train_df, y_pred=pred_labels_train))
-
-# %%
-print(classification_report(y_true=true_labels_test_df, y_pred=pred_labels_test))
-
-# %%
-from scipy.stats import randint
-
-dt_param_grid = {
-    "criterion": ["entropy", "gini", "log_loss"],
-    "splitter": ["best", "random"],
-    "max_depth": [2, 3, 5, 6, 7, 10, 12, None], # If None, then nodes are expanded until all leaves are pure or until all leaves contain less than min_samples_split samples.
-    "min_samples_split": randint(2, 51), # also float
-    "min_samples_leaf": randint(1, 51), # also float
-    # min_weight_fraction_leaf
-    # "max_features": [None, 2, 3, 4, 5]
-    #'random_state': RANDOM_STATE # ??
-    #'max_leaf_nodes'
-    #'min_impurity_decrease'
-    #'class_weight'
-    #'ccp_alpha'
-    }
-
-n_iter_search = 500 # number of random combinations to try
-n_jobs_search = -1 # use all cores
-n_folds = 5 # number of folds for cross validation
-best_score = make_scorer(f1_score) # score to use for selecting the best model
-
-dt_grid = RandomizedSearchCV(
-    tree.DecisionTreeClassifier(),
-    param_distributions=dt_param_grid,
-    n_iter=n_iter_search,
-    n_jobs=n_jobs_search,
-    scoring=best_score,
+clf_name = 'DecisionTreeClassifier'
+param_grid = {
+    "criterion": ["entropy", "gini", "log_loss"], # TODO: esplorarne altri
+    "max_depth": [2, 3, 5, 6, 7, 10, 12]
+}
+# TODO: capire se ci vuole troppe e in caso usare randomized
+grid = GridSearchCV( # uses a stratified 5-fold cv to validate the models
+    DecisionTreeClassifier(),
+    param_grid=param_grid,
+    n_jobs=-1,
+    scoring=make_scorer(f1_score),
     verbose=10,
-    cv=n_folds
+    cv=5,
+    refit=False
 )
-dt_grid.fit(indicators_train_df, true_labels_train_df)
-print(dt_grid.best_params_)
+grid.fit(indicators_train_df, true_labels_train_df)
 
 # %%
-dt = tree.DecisionTreeClassifier(**dt_grid.best_params_).fit(indicators_train_df, true_labels_train_df)
+# TODO: scegliere il miglior modello in modo più accurato, e.g.
+# - differisce tanto dagli altri? scikit learn offre qualcosa per test statistici?
+# - è in overfitting?
 
 # %%
-dot_data = tree.export_graphviz(
-    dt,
+cv_results_df = pd.DataFrame(grid.cv_results_)
+best_index = grid.best_index_
+best_model_params = cv_results_df.loc[best_index]['params']
+best_model_train_score = cv_results_df.loc[best_index]['mean_test_score']
+best_model = DecisionTreeClassifier(**best_model_params)
+# fit the model on all the training data
+fit_start = time()
+best_model.fit(indicators_train_df, true_labels_train)
+fit_time = time()-fit_start
+# get the predictions on the test data
+test_score_start = time()
+pred_labels_test = best_model.predict(indicators_test_df)
+test_score_time = time()-test_score_start
+pred_probas_test = best_model.predict_proba(indicators_test_df)
+# save the predictions
+pd.DataFrame(
+    {'labels': pred_labels_test, 'probs_True': pred_probas_test[:,1]}
+).to_csv(f'{RESULTS_DIR}/{clf_name}_preds.csv')
+
+# %%
+compute_clf_scores(
+    y_true=true_labels_test,
+    y_pred=pred_labels_test,
+    train_score=best_model_train_score,
+    train_score_name='f1_score_train', # TODO: migliorare formatting
+    train_time=fit_time,
+    score_time=test_score_time,
+    params=best_model_params,
+    prob_pred=pred_probas_test,
+    clf_name=clf_name,
+    path=f'{RESULTS_DIR}/{clf_name}_scores.csv'
+)
+
+# %%
+dot_data = export_graphviz(
+    best_model,
     out_file=None, 
     feature_names=list(indicators_train_df.columns),
     filled=True,
@@ -111,56 +100,59 @@ graph = pydotplus.graph_from_dot_data(dot_data)
 Image(graph.create_png())
 
 # %%
-plot_confusion_matrix(y_true=true_labels_test, y_pred=pred_labels_test)
+plot_confusion_matrix(
+    y_true=true_labels_test,
+    y_pred=pred_labels_test,
+    title=clf_name
+)
 
 # %%
 plot_predictions_in_features_space(
-    incidents_test_df,
-    ['n_males_prop', 'n_child_prop', 'n_participants'],
-    true_labels_test,
-    pred_labels_test,
+    df=incidents_test_df,
+    features=['n_males_prop', 'n_child_prop', 'n_participants'], # TODO: farlo con features significativve
+    true_labels=true_labels_test,
+    pred_labels=pred_labels_test,
     figsize=(15, 15)
 )
 
 # %%
-y_trues = [true_labels_test, true_labels_test]
-y_probs = [pred_probas_test[:,1], pred_probas_test[:,1]]
-names = ['DT 1', 'DT 2']
-plot_roc(y_trues, y_probs, names)
+plot_roc(y_trues=[true_labels_test], y_probs=[pred_probas_test[:,1]], names=[clf_name])
 
 # %%
-fig, axs = plt.subplots(1, 2, figsize=(10, 5))
-plot_PCA_decision_boundary(indicators_train_df, true_labels_train, dt, 'DT', axs=axs[0])
-plot_PCA_decision_boundary(indicators_train_df, true_labels_train, dt, 'DT', axs=axs[1])
-
-# %%
-fig, axs = plt.subplots(1, 2, figsize=(10, 5))
-plot_learning_curve(dt, 'DT', indicators_train_df, true_labels_train, axs[0])
-plot_learning_curve(dt, 'DT', indicators_train_df, true_labels_train, axs[1])
-
-# %%
-dt_param_grid = {
-    "criterion": ["entropy", "gini", "log_loss"],
-    "max_depth": [2, 3, 5, 6, 7, 10, 12]
-}
-
-dt_grid = GridSearchCV(
-    tree.DecisionTreeClassifier(),
-    param_grid=dt_param_grid,
-    n_jobs=-1, # default cv = stratified 5-fold
-    scoring=make_scorer(f1_score),
-    verbose=10,
-    cv=5
+fig, axs = plt.subplots(1, 1, figsize=(10, 5))
+plot_PCA_decision_boundary(
+  train_set=indicators_train_df,
+  train_label=true_labels_train,
+  classifier=best_model,
+  classifier_name=clf_name,
+  axs=axs
 )
-dt_grid.fit(indicators_train_df, true_labels_train_df)
+
+# %%
+fig, axs = plt.subplots(1, 1, figsize=(10, 5))
+plot_learning_curve(
+    classifier=best_model,
+    classifier_name=clf_name,
+    train_set=indicators_train_df,
+    labels=true_labels_train,
+    ax=axs,
+    train_sizes=np.linspace(0.1, 1.0, 5),
+    metric='f1'
+)
 
 # %%
 param_of_interest = 'max_depth'
-fixed_params = dt_grid.best_params_
+fixed_params = best_model_params.copy()
 fixed_params.pop(param_of_interest)
 fig, axs = plt.subplots(1, 1, figsize=(10, 5))
-plot_scores_varying_params(dt_grid.cv_results_, param_of_interest, fixed_params, 'F1 Score', axs)
-plot_scores_varying_params(dt_grid.cv_results_, param_of_interest, {'criterion': 'entropy'}, 'F1 Score', axs, title=f'F1 score varying {param_of_interest}')
+plot_scores_varying_params(
+    grid.cv_results_,
+    param_of_interest,
+    fixed_params,
+    'F1', # TODO: specificare l'averaging che usa
+    axs,
+    title=clf_name
+)
 
 # %%
 plot_distribution_missclassifications(true_labels_test, pred_labels_test, incidents_test_df, 'n_killed', 'bar')
