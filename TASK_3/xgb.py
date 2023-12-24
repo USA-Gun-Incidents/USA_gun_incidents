cccf5726c1cb0d76ff99cbdd0001ea957e24666c
iretes <i.testa@studenti.unipi.it>
2023-12-24 18:25:38 +0100

    xgb

---
+++ TASK_3/xgb.py
@@ -0,0 +1,243 @@
+# %%
+import pandas as pd
+import json
+from classification_utils import *
+from time import time
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import make_scorer, f1_score
+from xgboost import XGBClassifier
+from xgboost import plot_tree
+import pickle
+RESULTS_DIR = '../data/classification_results'
+clf_name = 'XGBClassifier'
+
+# %%
+# load the data
+incidents_train_df = pd.read_csv('../data/clf_indicators_train.csv', index_col=0)
+incidents_test_df = pd.read_csv('../data/clf_indicators_test.csv', index_col=0)
+true_labels_train_df = pd.read_csv('../data/clf_y_train.csv', index_col=0)
+true_labels_train = true_labels_train_df.values.ravel()
+true_labels_test_df = pd.read_csv('../data/clf_y_test.csv', index_col=0)
+true_labels_test = true_labels_test_df.values.ravel()
+
+# load the names of the features to use for the classification task
+features_for_clf = json.loads(open('../data/clf_indicators_subset.json').read())
+
+# project on the features_to_use
+indicators_train_df = incidents_train_df[features_for_clf]
+indicators_test_df = incidents_test_df[features_for_clf]
+
+# %%
+# TODO: decidere quali parametri esplorare
+param_grid = { # https://xgboost.readthedocs.io/en/stable/parameter.html
+    'booster': ['gbtree'],
+    'eta': [0.001, 0.01, 0.1, 0.3], # i.e. learning_rate in [0,1], default = 0.3
+    'gamma': [0],
+    'max_depth': [6],
+    'min_child_weight': [1],
+    'max_delta_step': [0],
+    'subsample': [1],
+    'sampling_method': ['uniform'],
+    'colsample_bytree': [1],
+    'colsample_bylevel': [1],
+    'colsample_bynode': [1],
+    'lambda': [1],
+    'alpha': [0],
+    'tree_method': ['auto'],
+    'scale_pos_weight': [1],
+    'refresh_leaf': [1],
+    'grow_policy': ['depthwise'],
+    'max_leaves': [0],
+    # max_cat_to_onehot ?
+}
+gs = GridSearchCV(
+    XGBClassifier(),
+    param_grid=param_grid,
+    n_jobs=-1,
+    scoring=make_scorer(f1_score),
+    verbose=10,
+    cv=5, # uses a stratified 5-fold cv to validate the models
+    refit=False
+)
+gs.fit(indicators_train_df, true_labels_train)
+
+# %%
+cv_results_df = pd.DataFrame(gs.cv_results_)
+cv_results_df.head()
+
+# %%
+# TODO: heatmaps params
+
+# %%
+params = [col for col in cv_results_df.columns if 'param_' in col and 'random' not in col]
+cv_results_df.sort_values(
+    by='mean_test_score',
+    ascending=False)[params+['std_test_score', 'mean_test_score']].head(20).style.background_gradient(subset=['std_test_score', 'mean_test_score'], cmap='Blues')
+
+# %%
+best_index = gs.best_index_
+best_model_params = cv_results_df.loc[best_index]['params']
+best_model = XGBClassifier(**best_model_params)
+
+# fit the model on all the training data
+fit_start = time()
+best_model.fit(indicators_train_df, true_labels_train)
+fit_time = time()-fit_start
+
+# get the predictions on the training data
+train_score_start = time()
+pred_labels_train = best_model.predict(indicators_train_df)
+train_score_time = time()-train_score_start
+pred_probas_train = best_model.predict_proba(indicators_train_df)
+
+# get the predictions on the test data
+test_score_start = time()
+pred_labels_test = best_model.predict(indicators_test_df)
+test_score_time = time()-test_score_start
+pred_probas_test = best_model.predict_proba(indicators_test_df)
+
+# save the predictions
+pd.DataFrame(
+    {'labels': pred_labels_test, 'probs': pred_probas_test[:,1]}
+).to_csv(f'{RESULTS_DIR}/{clf_name}_preds.csv')
+
+# save the model
+file = open(f'{RESULTS_DIR}/{clf_name}.pkl', 'wb')
+pickle.dump(obj=best_model, file=file)
+file.close()
+
+# save the cv results
+best_model_cv_results = pd.DataFrame(cv_results_df.iloc[best_index]).T
+best_model_cv_results.index = [clf_name]
+best_model_cv_results.to_csv(f'{RESULTS_DIR}/{clf_name}_train_cv_scores.csv')
+
+# %%
+compute_clf_scores(
+    y_true=true_labels_train,
+    y_pred=pred_labels_train,
+    train_time=fit_time,
+    score_time=train_score_time,
+    params=best_model_params,
+    prob_pred=pred_probas_train,
+    clf_name=clf_name,
+    path=f'{RESULTS_DIR}/{clf_name}_train_scores.csv'
+)
+
+# %%
+compute_clf_scores(
+    y_true=true_labels_test,
+    y_pred=pred_labels_test,
+    train_time=fit_time,
+    score_time=test_score_time,
+    params=best_model_params,
+    prob_pred=pred_probas_test,
+    clf_name=clf_name,
+    path=f'{RESULTS_DIR}/{clf_name}_test_scores.csv'
+)
+
+# %%
+fig, axs = plt.subplots(figsize=(20, 50))
+plot_tree(best_model, num_trees=0, ax=axs)
+
+# %%
+fig, axs = plt.subplots(figsize=(20, 40))
+plot_tree(best_model, num_trees=1, ax=axs)
+
+# %%
+plot_confusion_matrix(
+    y_true=true_labels_test,
+    y_pred=pred_labels_test,
+    title=clf_name
+)
+
+# %%
+plot_roc(y_true=true_labels_test, y_probs=[pred_probas_test[:,1]], names=[clf_name])
+
+# %%
+plot_predictions_in_features_space(
+    df=incidents_test_df,
+    features=['n_males_prop', 'n_child_prop', 'n_participants'], # TODO: farlo con features significativve
+    true_labels=true_labels_test,
+    pred_labels=pred_labels_test,
+    figsize=(15, 15)
+)
+
+# %%
+fig, axs = plt.subplots(1, 1, figsize=(10, 5))
+plot_PCA_decision_boundary(
+  train_set=indicators_train_df,
+  train_label=true_labels_train,
+  classifier=best_model,
+  classifier_name=clf_name,
+  axs=axs
+)
+
+# %%
+fig, axs = plt.subplots(1, 1, figsize=(10, 5))
+plot_learning_curve(
+    classifier=best_model,
+    classifier_name=clf_name,
+    train_set=indicators_train_df,
+    labels=true_labels_train,
+    ax=axs,
+    train_sizes=np.linspace(0.1, 1.0, 5),
+    metric='f1'
+)
+
+# %%
+# TODO: plot al variare di parametri di complessità
+
+# %%
+plot_distribution_missclassifications(
+    true_labels_test,
+    pred_labels_test,
+    incidents_test_df,
+    'n_killed',
+    'bar',
+    title='n_killed distribution'
+)
+
+# %%
+plot_distribution_missclassifications(
+    true_labels_test,
+    pred_labels_test,
+    incidents_test_df,
+    'suicide',
+    'pie',
+    title='suicide distribution'
+)
+
+# %%
+plot_distribution_missclassifications(
+    true_labels_test,
+    pred_labels_test,
+    incidents_test_df,
+    'incident_characteristics1',
+    'pie',
+    title='incident_characteristics1 distribution'
+)
+
+# %%
+plot_distribution_missclassifications(
+    true_labels_test,
+    pred_labels_test,
+    incidents_test_df,
+    'incident_characteristics2',
+    'pie',
+    pie_perc_threshold=2,
+    figsize=(20, 5),
+    title='incident_characteristics2 distribution'
+)
+
+# %%
+plot_distribution_missclassifications(
+    true_labels_test,
+    pred_labels_test,
+    incidents_test_df,
+    'location_imp',
+    'hist',
+    bins=5,
+    title='location_imp distribution'
+)
+
+
