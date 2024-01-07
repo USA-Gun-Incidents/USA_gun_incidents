72be24aa52956c19fb21963fd96812040de22d3c
LucaMaro1 <lucamariniwork@gmail.com>
2024-01-07 02:37:36 +0100

    nearest centroid definitivo

--- TASK_3/nc.py
+++ TASK_3/nc.py
@@ -55,14 +55,6 @@ print(f'Number of features: {len(features_for_clf)}')
 # il centroide sta nel mezzo => la feature non è rilevante)
 
 # %% [markdown]
-# We make a random oversampling on the positive class such that fatal incidents become 40% of the total number of records. Doing oversampling we can help classifier make better predictions on test set, but we don't reach a 50-50 ratio since it may be unrealistic
-
-# %%
-# minority oversampling
-oversampler = RandomOverSampler(sampling_strategy=0.67, random_state=SEED) # num_minority (40%) / num_majority (60%) = 0.67
-indicators_oversampled_train_df, true_oversampled_labels_train = oversampler.fit_resample(indicators_train_df, true_labels_train)
-
-# %% [markdown]
 # We make a 5-fold cross validation in which we check which of the two distance metrics (euclidean and manhattan) has better performance. <br>
 # The values for training are scaled to be between 0 and 1 so that bigger values in module, and so bigger distances, don't affect the classification.
 
@@ -85,7 +77,7 @@ gs = GridSearchCV(
     cv=cv,
     refit=False
 )
-gs.fit(indicators_oversampled_train_df, true_oversampled_labels_train)
+gs.fit(indicators_train_df, true_labels_train)
 
 # %% [markdown]
 # We display some metrics derived from cross validation fitting.
@@ -105,6 +97,20 @@ cv_results_df.sort_values(
     ascending=False)[params+['std_test_score', 'mean_test_score']].head(20).style.background_gradient(subset=['std_test_score', 'mean_test_score'], cmap='Blues')
 
 # %% [markdown]
+# We make a random oversampling on the positive class such that fatal incidents become 40% of the total number of records. Doing oversampling we can help classifier make better predictions on test set, but we don't reach a 50-50 ratio since it may be unrealistic.
+
+# %%
+# minority oversampling
+indicators_oversampled_train_df = pd.read_csv('../data/clf_indicators_train_over.csv', index_col=0)
+indicators_oversampled_train_df = indicators_oversampled_train_df[features_for_clf]
+true_oversampled_labels_train = pd.read_csv('../data/clf_y_train_over.csv', index_col=0).values.ravel()
+
+# SMOTE oversampling
+indicators_smote_train_df = pd.read_csv('../data/clf_indicators_train_smote.csv', index_col=0)
+indicators_smote_train_df = indicators_smote_train_df[features_for_clf]
+true_labels_smote_train = pd.read_csv('../data/clf_y_train_smote.csv', index_col=0).values.ravel()
+
+# %% [markdown]
 # We fit the best model we got from cross validation, then we make and save the predictions on test set. Finally we save the models so that we can reuse it without performing the training.
 
 # %%
@@ -112,35 +118,94 @@ best_index = gs.best_index_
 best_model_params = cv_results_df.loc[best_index]['params']
 best_model_params = {k.replace('nc__', ''): v for k, v in best_model_params.items()}
 best_model = NearestCentroid(**best_model_params)
+best_model_oversampled = NearestCentroid(**best_model_params)
+best_model_smote = NearestCentroid(**best_model_params)
 
 # scale all the data
 minmax_scaler = MinMaxScaler()
 indicators_train_scaled = minmax_scaler.fit_transform(indicators_train_df)
 indicators_oversampled_train_scaled = minmax_scaler.fit_transform(indicators_oversampled_train_df)
+indicators_smote_train_scaled = minmax_scaler.fit_transform(indicators_smote_train_df)
+indicators_test_scaled = minmax_scaler.fit_transform(indicators_test_df)
+
+
+# fit the model on all the training data
+fit_start = time()
+best_model.fit(indicators_train_scaled, true_labels_train)
+fit_time = time()-fit_start
 
 # fit the model on all the training data (oversampled)
 fit_start = time()
-best_model.fit(indicators_oversampled_train_scaled, true_oversampled_labels_train)
-fit_time = time()-fit_start
+best_model_oversampled.fit(indicators_oversampled_train_scaled, true_oversampled_labels_train)
+fit_time_oversampled = time()-fit_start
+
+# fit the model on all the training data (smote)
+fit_start = time()
+best_model_smote.fit(indicators_smote_train_scaled, true_labels_smote_train)
+fit_time_smote = time()-fit_start
+
 
 # get the predictions on the training data
 train_score_start = time()
 pred_labels_train = best_model.predict(indicators_train_scaled)
 train_score_time = time()-train_score_start
 
+# get the predictions on the training data (oversampled)
+train_score_start = time()
+pred_labels_oversampled_train = best_model_oversampled.predict(indicators_oversampled_train_scaled)
+train_score_time_oversampled = time()-train_score_start
+
+# get the predictions on the training data (smote)
+train_score_start = time()
+pred_labels_smote_train = best_model_smote.predict(indicators_smote_train_scaled)
+train_score_time_smote = time()-train_score_start
+
+
 # get the predictions on the test data
 test_score_start = time()
-pred_labels_test = best_model.predict(indicators_test_df.values)
+pred_labels_test = best_model.predict(indicators_test_scaled)
 test_score_time = time()-test_score_start
+
+# get the predictions on the test data (oversampled)
+test_score_start = time()
+pred_labels_oversampled_test = best_model_oversampled.predict(indicators_test_scaled)
+test_score_time_oversampled = time()-test_score_start
+
+# get the predictions on the test data (smote)
+test_score_start = time()
+pred_labels_smote_test = best_model_smote.predict(indicators_test_scaled)
+test_score_time_smote = time()-test_score_start
+
 
 # save the predictions
 pd.DataFrame(
     {'labels': pred_labels_test}
 ).to_csv(f'{RESULTS_DIR}/{clf_name}_preds.csv')
 
+# save the predictions (oversampled)
+pd.DataFrame(
+    {'labels': pred_labels_oversampled_test}
+).to_csv(f'{RESULTS_DIR}/{clf_name}_oversampled_preds.csv')
+
+# save the predictions (smote)
+pd.DataFrame(
+    {'labels': pred_labels_smote_test}
+).to_csv(f'{RESULTS_DIR}/{clf_name}_smote_preds.csv')
+
+
 # save the model
 file = open(f'{RESULTS_DIR}/{clf_name}.pkl', 'wb')
 pickle.dump(obj=best_model, file=file)
+file.close()
+
+# save the model
+file = open(f'{RESULTS_DIR}/{clf_name}_oversampled.pkl', 'wb')
+pickle.dump(obj=best_model_oversampled, file=file)
+file.close()
+
+# save the model
+file = open(f'{RESULTS_DIR}/{clf_name}_smote.pkl', 'wb')
+pickle.dump(obj=best_model_smote, file=file)
 file.close()
 
 # save the cv results
@@ -149,7 +214,7 @@ best_model_cv_results.index = [clf_name]
 best_model_cv_results.to_csv(f'{RESULTS_DIR}/{clf_name}_train_cv_scores.csv')
 
 # %% [markdown]
-# We display some classification scores in order to have some metrics useful to make comparisions with other models. These scores are refearing to predictions on training and test set
+# We display some classification scores in order to have some metrics useful to make comparisions with other models. These scores are refearing to predictions on training and test set including oversampled and smote.
 
 # %%
 compute_clf_scores(
@@ -165,6 +230,30 @@ compute_clf_scores(
 
 # %%
 compute_clf_scores(
+    y_true=true_oversampled_labels_train,
+    y_pred=pred_labels_oversampled_train,
+    train_time=fit_time_oversampled,
+    score_time=train_score_time_oversampled,
+    params=best_model_params,
+    prob_pred=None,
+    clf_name=clf_name,
+    path=f'{RESULTS_DIR}/{clf_name}_oversampled_train_scores.csv'
+)
+
+# %%
+compute_clf_scores(
+    y_true=true_labels_smote_train,
+    y_pred=pred_labels_smote_train,
+    train_time=fit_time_smote,
+    score_time=train_score_time_smote,
+    params=best_model_params,
+    prob_pred=None,
+    clf_name=clf_name,
+    path=f'{RESULTS_DIR}/{clf_name}_smote_train_scores.csv'
+)
+
+# %%
+compute_clf_scores(
     y_true=true_labels_test,
     y_pred=pred_labels_test,
     train_time=fit_time,
@@ -175,36 +264,57 @@ compute_clf_scores(
     path=f'{RESULTS_DIR}/{clf_name}_test_scores.csv'
 )
 
+# %%
+compute_clf_scores(
+    y_true=true_labels_test,
+    y_pred=pred_labels_oversampled_test,
+    train_time=fit_time_oversampled,
+    score_time=train_score_time_oversampled,
+    params=best_model_params,
+    prob_pred=None,
+    clf_name=clf_name,
+    path=f'{RESULTS_DIR}/{clf_name}_oversampled_test_scores.csv'
+)
+
+# %%
+compute_clf_scores(
+    y_true=true_labels_test,
+    y_pred=pred_labels_smote_test,
+    train_time=fit_time_smote,
+    score_time=train_score_time_smote,
+    params=best_model_params,
+    prob_pred=None,
+    clf_name=clf_name,
+    path=f'{RESULTS_DIR}/{clf_name}_smote_test_scores.csv'
+)
+
+# %% [markdown]
+# Since the model with better performance (better f1-score macro) is the one trained on oversampled data, we will use it for further analisys.
+
 # %% [markdown]
 # We plot the diffusion matrix in orther to know what is the balancing between false positves, false negatives, true positives and true negatives.
 
 # %%
 plot_confusion_matrix(
     y_true=true_labels_test,
-    y_pred=pred_labels_test,
+    y_pred=pred_labels_oversampled_test,
     title=clf_name
 )
 
 # %% [markdown]
-# As we can see, almost all the features are classified as non fatal, so we have a lot of TN but too few TP. This leads, as enlighted by classification scores, to a bad precision and an awful recall for fatal incidents.
-
-# %% [markdown]
 # We plot the classification labels in the bidimensional feature spaces obtained pairing 3 features: **aggression**, **drug_alcohol** and **organized**.
-
-# %%
-incidents_test_df.columns
 
 # %%
 plot_predictions_in_features_space(
     df=incidents_test_df,
     features=['aggression', 'drug_alcohol', 'gun_law_rank', 'n_males'], # TODO: farlo con features significativve
     true_labels=true_labels_test,
-    pred_labels=pred_labels_test,
+    pred_labels=pred_labels_oversampled_test,
     figsize=(30, 30)
 )
 
 # %% [markdown]
-# The plot is not very much informative, since the number clusers don't seem to be well separated in the feature spaces. Moreover, we already know the classifier predicts almost every time 'non fatal', so we're not even able to see what are the few elements classified differently. There are just a few cases where there's a visible diffence in the amount of incidents classified as fatal for certain values, in particular in features spaces reguarding **aggression** and **drug_alcohol**.
+# Given the plot, although we can't deduce anything on the separation of clusters, we can notice that when **aggression** and **drug_alcohol** are set to 1, the incidents are not fatal, and the model tends to predict them well.
 
 # %% [markdown]
 # We permorm PCA and we plot the decision boundary of the first two components.
@@ -212,10 +322,10 @@ plot_predictions_in_features_space(
 # %%
 fig, axs = plt.subplots(1, 1, figsize=(10, 5))
 plot_PCA_decision_boundary(
-  train_set=indicators_train_scaled,
+  train_set=indicators_oversampled_train_scaled,
   features=indicators_train_df.columns, # TODO: eventualmente usare solo le numeriche
-  train_label=true_labels_train,
-  classifier=best_model,
+  train_label=true_oversampled_labels_train,
+  classifier=best_model_oversampled,
   classifier_name=clf_name,
   axs=axs
 )
@@ -224,30 +334,30 @@ plot_PCA_decision_boundary(
 # As we expected we got a very simple decision boundary, since the two regions are separated by a straight line.
 
 # %% [markdown]
-# We plot the learning curve of the model. Each time we perform training in a subsample of the dataset a see the results vary.
+# We plot the learning curve of the model. Each time we perform training in a subsample of the dataset and see how the results vary.
 
 # %%
 fig, axs = plt.subplots(1, 1, figsize=(10, 5))
 plot_learning_curve(
-    classifier=best_model,
+    classifier=best_model_oversampled,
     classifier_name=clf_name,
-    train_set=indicators_train_scaled,
-    labels=true_labels_train,
+    train_set=indicators_oversampled_train_scaled,
+    labels=true_oversampled_labels_train,
     ax=axs,
     train_sizes=np.linspace(0.1, 1.0, 5),
     metric='f1'
 )
 
 # %% [markdown]
-# We can see that both the training and test set F1-score is higher with less training samples, but while the curve of test score is increasing after a certain point, the curve of training still continue to decrese.
+# We notice that the model has a better performance on test than on train. The curve of the test score is pretty stable around 0.6 and has a small standard deviation, while the training curve has some big improvements, both on F1-score and standard deviation, as the training set increase. 
 
 # %% [markdown]
-# We display with different types of plot the distribution of the values of the miscassified incidents of **n_killed**, **suicide**, **aggression**, **road** and **location_imp**
+# We display with different types of plot the distribution of the values of the miscassified incidents of **n_killed**, **suicide**, **aggression**, **road** and **location_imp**.
 
 # %%
 plot_distribution_missclassifications(
     true_labels_test,
-    pred_labels_test,
+    pred_labels_oversampled_test,
     incidents_test_df,
     'n_killed',
     'bar',
@@ -257,7 +367,7 @@ plot_distribution_missclassifications(
 # %%
 plot_distribution_missclassifications(
     true_labels_test,
-    pred_labels_test,
+    pred_labels_oversampled_test,
     incidents_test_df,
     'suicide',
     'pie',
@@ -267,7 +377,7 @@ plot_distribution_missclassifications(
 # %%
 plot_distribution_missclassifications(
     true_labels_test,
-    pred_labels_test,
+    pred_labels_oversampled_test,
     incidents_test_df,
     'aggression',
     'pie',
@@ -277,7 +387,7 @@ plot_distribution_missclassifications(
 # %%
 plot_distribution_missclassifications(
     true_labels_test,
-    pred_labels_test,
+    pred_labels_oversampled_test,
     incidents_test_df,
     'road',
     'pie',
@@ -289,7 +399,7 @@ plot_distribution_missclassifications(
 # %%
 plot_distribution_missclassifications(
     true_labels_test,
-    pred_labels_test,
+    pred_labels_oversampled_test,
     incidents_test_df,
     'location_imp',
     'hist',
@@ -298,10 +408,10 @@ plot_distribution_missclassifications(
 )
 
 # %% [markdown]
-# We can notice that for **location_imp** we have similar distribuition shapes. For **road** and **aggression** we have that the percentage of misclassified incidents with tag set to True is less than the one on the entire dataset; for **suicide** instead, we have the opposite situation. For **n_killed** we have a different situation, where the two distributions are quite similar, except that there are no miscassified incidents with the attribute equal to 0
+# We can notice that for **location_imp** and **n_killed** we have similar distribuition shapes. For **road**, **aggression**  and **suicide** we have that the percentage of misclassified incidents with the tag set to True is less than the one on the entire dataset, in particular there are very few miscassified incidents with **suicide** tag set to true.
 
 # %% [markdown]
 # ## Final considerations
-# The advantage of Nearest Centroid is that is one of the simplest distance based model for classification, but of course this brings some drawbacks. In fact, in cases like ours where we have a non-trivial dataset, NC tends to be a bad classfier. This is confirmed by our analisys, where the model managed to classify correctly as fatal only a very small amount of incidents.
+# The advantage of Nearest Centroid is that is one of the simplest distance based model for classification, on the other hand it's difficult to have good performance on non-trivial dataset like the one of our case study. So, better performance are possible with more complex models.
 
 
